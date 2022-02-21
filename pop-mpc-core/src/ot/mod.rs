5c94811b497e59fe7c268b4370fdb448ed4990ef
sinuio <>
2022-02-20 22:47:49 -0800

    default sender and receiver

--- pop-mpc-core/src/ot/mod.rs
+++ pop-mpc-core/src/ot/mod.rs
@@ -3,4 +3,22 @@ pub mod errors;
 pub mod extension;
 
 pub use base::*;
+pub use errors::*;
 pub use extension::*;
+
+use aes::cipher::{generic_array::GenericArray, NewBlockCipher};
+use aes::Aes128;
+use rand::{CryptoRng, Rng, SeedableRng};
+use rand_chacha::ChaCha12Rng;
+
+pub fn default_sender() -> OTSender<ChaCha12Rng, Aes128> {
+    let rng = ChaCha12Rng::from_entropy();
+    let cipher = Aes128::new(GenericArray::from_slice(&[0u8; 16]));
+    OTSender::new(rng, cipher)
+}
+
+pub fn default_receiver() -> OTReceiver<ChaCha12Rng, Aes128> {
+    let rng = ChaCha12Rng::from_entropy();
+    let cipher = Aes128::new(GenericArray::from_slice(&[0u8; 16]));
+    OTReceiver::new(rng, cipher)
+}
