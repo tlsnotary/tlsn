8fb57b66e8d7f4d8f19106c8544c7c65b215b459
sinuio <>
2022-02-24 23:12:54 -0800

    removed unused

--- pop-mpc-proto/build.rs
+++
@@ -1,5 +0,0 @@
-use std::io::Result;
-fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/core.proto", "src/ot.proto"], &["src/"])?;
-    Ok(())
-}
--- pop-mpc-proto/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod pop {
-    pub mod mpc {
-        include!(concat!(env!("OUT_DIR"), "/pop.mpc.rs"));
-        pub mod ot {
-            include!(concat!(env!("OUT_DIR"), "/pop.mpc.ot.rs"));
-        }
-    }
-}
