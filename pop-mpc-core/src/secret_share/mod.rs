3bfaedd46e523f7e6246832263fe6618ab52ab66
sinuio <>
2022-03-15 23:15:47 -0700

    removed unused import

--- pop-mpc-core/src/secret_share/mod.rs
+++ pop-mpc-core/src/secret_share/mod.rs
@@ -13,7 +13,7 @@ mod tests {
     use curv::{arithmetic::Converter, BigInt};
     use elliptic_curve::sec1::ToEncodedPoint;
     use p256::SecretKey;
-    use rand::{thread_rng, CryptoRng};
+    use rand::thread_rng;
 
     #[test]
     fn test_secret_share() {
