fe427a2662c8374eca9d705d66012a40787aa979
sinu.eth <65924192+sinui0@users.noreply.github.com>
2022-05-16 11:07:10 -0700

    ot aio refactor (#28)
    Co-authored-by: sinuio <>

--- mpc-aio/src/ot/base/sender.rs
+++ mpc-aio/src/ot/base/sender.rs
@@ -1,15 +1,19 @@
 use super::OTError;
 use async_trait::async_trait;
-use futures_util::{Sink, SinkExt, Stream, StreamExt};
+use futures_util::{SinkExt, StreamExt};
 use mpc_core::ot::{Message, SendCore};
+use mpc_core::proto::ot::Message as ProtoMessage;
 use mpc_core::Block;
 use std::io::Error as IOError;
 use std::io::ErrorKind;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio_util::codec::Framed;
 use tracing::{instrument, trace};
+use utils_aio::codec::ProstCodecDelimited;
 
 pub struct Sender<OT, S> {
     ot: OT,
-    stream: S,
+    stream: Framed<S, ProstCodecDelimited<Message, ProtoMessage>>,
 }
 
 #[async_trait]
@@ -17,29 +21,27 @@ pub trait OTSend {
     async fn send(&mut self, payload: &[[Block; 2]]) -> Result<(), OTError>;
 }
 
-impl<
-        OT: SendCore + Send,
-        S: Sink<Message> + Stream<Item = Result<Message, E>> + Send + Unpin,
-        E: std::fmt::Debug,
-    > Sender<OT, S>
+impl<OT, S> Sender<OT, S>
 where
-    OTError: From<<S as Sink<Message>>::Error>,
-    OTError: From<E>,
+    OT: SendCore + Send,
+    S: AsyncRead + AsyncWrite + Send + Unpin,
 {
     pub fn new(ot: OT, stream: S) -> Self {
-        Self { ot, stream }
+        Self {
+            ot,
+            stream: Framed::new(
+                stream,
+                ProstCodecDelimited::<Message, ProtoMessage>::default(),
+            ),
+        }
     }
 }
 
 #[async_trait]
-impl<
-        OT: SendCore + Send,
-        S: Sink<Message> + Stream<Item = Result<Message, E>> + Send + Unpin,
-        E: std::fmt::Debug,
-    > OTSend for Sender<OT, S>
+impl<OT, S> OTSend for Sender<OT, S>
 where
-    OTError: From<<S as Sink<Message>>::Error>,
-    OTError: From<E>,
+    OT: SendCore + Send,
+    S: AsyncRead + AsyncWrite + Send + Unpin,
 {
     #[instrument(skip(self, payload))]
     async fn send(&mut self, payload: &[[Block; 2]]) -> Result<(), OTError> {
--- mpc-aio/src/ot/extension/receiver.rs
+++ mpc-aio/src/ot/extension/receiver.rs
@@ -1,15 +1,19 @@
 use super::OTError;
 use async_trait::async_trait;
-use futures_util::{Sink, SinkExt, Stream, StreamExt};
+use futures_util::{SinkExt, StreamExt};
 use mpc_core::ot::{ExtReceiveCore, Message};
+use mpc_core::proto::ot::Message as ProtoMessage;
 use mpc_core::Block;
 use std::io::Error as IOError;
 use std::io::ErrorKind;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio_util::codec::Framed;
 use tracing::{instrument, trace};
+use utils_aio::codec::ProstCodecDelimited;
 
 pub struct ExtReceiver<OT, S> {
     ot: OT,
-    stream: S,
+    stream: Framed<S, ProstCodecDelimited<Message, ProtoMessage>>,
 }
 
 #[async_trait]
@@ -17,29 +21,27 @@ pub trait ExtOTReceive {
     async fn receive(&mut self, choice: &[bool]) -> Result<Vec<Block>, OTError>;
 }
 
-impl<
-        OT: ExtReceiveCore + Send,
-        S: Sink<Message> + Stream<Item = Result<Message, E>> + Send + Unpin,
-        E: std::fmt::Debug,
-    > ExtReceiver<OT, S>
+impl<OT, S> ExtReceiver<OT, S>
 where
-    OTError: From<<S as Sink<Message>>::Error>,
-    OTError: From<E>,
+    OT: ExtReceiveCore + Send,
+    S: AsyncRead + AsyncWrite + Send + Unpin,
 {
     pub fn new(ot: OT, stream: S) -> Self {
-        Self { ot, stream }
+        Self {
+            ot,
+            stream: Framed::new(
+                stream,
+                ProstCodecDelimited::<Message, ProtoMessage>::default(),
+            ),
+        }
     }
 }
 
 #[async_trait]
-impl<
-        OT: ExtReceiveCore + Send,
-        S: Sink<Message> + Stream<Item = Result<Message, E>> + Send + Unpin,
-        E: std::fmt::Debug,
-    > ExtOTReceive for ExtReceiver<OT, S>
+impl<OT, S> ExtOTReceive for ExtReceiver<OT, S>
 where
-    OTError: From<<S as Sink<Message>>::Error>,
-    OTError: From<E>,
+    OT: ExtReceiveCore + Send,
+    S: AsyncRead + AsyncWrite + Send + Unpin,
 {
     #[instrument(skip(self, choice))]
     async fn receive(&mut self, choice: &[bool]) -> Result<Vec<Block>, OTError> {
