ac36837ffc3cc95a22d80567957efd926c1a2b9d
sinuio <>
2022-06-07 12:36:47 -0700

    update handshake trait

--- tls-aio/src/handshaker.rs
+++ tls-aio/src/handshaker.rs
@@ -1,6 +1,6 @@
-use tls_core::msgs::{
-    handshake::{KeyShareEntry, Random},
-    message::OpaqueMessage,
+use tls_core::{
+    key::PublicKey,
+    msgs::{handshake::Random, message::PlainMessage},
 };
 
 use async_trait::async_trait;
@@ -13,17 +13,17 @@ pub trait Handshake: Send + Sync {
     /// Returns client_random value
     async fn client_random(&mut self) -> Result<Random, Self::Error>;
     /// Returns public client keyshare
-    async fn initial_key_share(&mut self) -> Result<KeyShareEntry, Self::Error>;
-    /// Receives server random
-    async fn receive_server_random(&mut self, random: Random) -> Result<(), Self::Error>;
-    /// Receives server keyshare
-    async fn receive_server_key_share(&mut self, key: KeyShareEntry) -> Result<(), Self::Error>;
-    /// Receive handshake hash at ServerHello
-    async fn receive_hs_hash_server_hello(&mut self, hash: &[u8]) -> Result<(), Self::Error>;
-    /// Returns expected ServerFinished
+    async fn client_key_share(&mut self) -> Result<PublicKey, Self::Error>;
+    /// Sets server random
+    async fn set_server_random(&mut self, random: Random) -> Result<(), Self::Error>;
+    /// Sets server keyshare
+    async fn set_server_key_share(&mut self, key: PublicKey) -> Result<(), Self::Error>;
+    /// Sets handshake hash at ServerHello
+    async fn set_hs_hash_server_hello(&mut self, hash: &[u8]) -> Result<(), Self::Error>;
+    /// Returns expected ServerFinished verify_data
     async fn server_finished(&mut self, hash: &[u8]) -> Result<Vec<u8>, Self::Error>;
-    /// Returns ClientFinished
-    async fn client_finished(&mut self, hash: &[u8]) -> Result<OpaqueMessage, Self::Error>;
+    /// Returns ClientFinished verify_data
+    async fn client_finished(&mut self, hash: &[u8]) -> Result<Vec<u8>, Self::Error>;
     /// Returns initialized MessageEncrypter
     async fn message_encrypter(
         &mut self,
