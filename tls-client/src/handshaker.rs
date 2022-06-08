e7f196d1a5fb777718d6405c7f9aeb30d194263c
sinuio <>
2022-06-08 09:32:23 -0700

    fix InvalidHandshaker

--- tls-client/src/handshaker.rs
+++ tls-client/src/handshaker.rs
@@ -3,10 +3,8 @@ use tls_aio::{
     cipher::{MessageDecrypter, MessageEncrypter},
     handshaker::Handshake,
 };
-use tls_core::msgs::{
-    handshake::{KeyShareEntry, Random},
-    message::OpaqueMessage,
-};
+use tls_core::key::PublicKey;
+use tls_core::msgs::handshake::Random;
 
 use async_trait::async_trait;
 
@@ -18,22 +16,22 @@ impl Handshake for InvalidHandShaker {
     async fn client_random(&mut self) -> Result<Random, Error> {
         Err(Error::General("handshaker not yet available".to_string()))
     }
-    async fn initial_key_share(&mut self) -> Result<KeyShareEntry, Error> {
+    async fn client_key_share(&mut self) -> Result<PublicKey, Error> {
         Err(Error::General("handshaker not yet available".to_string()))
     }
-    async fn receive_server_random(&mut self, _random: Random) -> Result<(), Self::Error> {
+    async fn set_server_random(&mut self, _random: Random) -> Result<(), Self::Error> {
         Err(Error::General("handshaker not yet available".to_string()))
     }
-    async fn receive_server_key_share(&mut self, _key: KeyShareEntry) -> Result<(), Self::Error> {
+    async fn set_server_key_share(&mut self, _key: PublicKey) -> Result<(), Self::Error> {
         Err(Error::General("handshaker not yet available".to_string()))
     }
-    async fn receive_hs_hash_server_hello(&mut self, _hash: &[u8]) -> Result<(), Self::Error> {
+    async fn set_hs_hash_server_hello(&mut self, _hash: &[u8]) -> Result<(), Self::Error> {
         Err(Error::General("handshaker not yet available".to_string()))
     }
     async fn server_finished(&mut self, _hash: &[u8]) -> Result<Vec<u8>, Self::Error> {
         Err(Error::General("handshaker not yet available".to_string()))
     }
-    async fn client_finished(&mut self, _hash: &[u8]) -> Result<OpaqueMessage, Self::Error> {
+    async fn client_finished(&mut self, _hash: &[u8]) -> Result<Vec<u8>, Self::Error> {
         Err(Error::General("handshaker not yet available".to_string()))
     }
     async fn message_encrypter(
