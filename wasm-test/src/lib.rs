e75963ec3af9d2ea765dd88f960554369adec5ef
th4s <th4s@metavoid.xyz>
2022-07-28 12:53:17 +0200

    WIP: Matrix transposition
    - bugfix in transpose_bytes implementation
    - added test case
    - return error for N = 1 case

--- wasm-test/src/lib.rs
+++ wasm-test/src/lib.rs
@@ -11,22 +11,34 @@ use thiserror::Error;
 ///
 /// We use the following algorithm:
 /// https://docs.rs/oblivious-transfer/latest/oblivious_transfer/extension/fn.transpose128.html
-pub fn transpose(matrix: &mut [u8], rows: u32) -> Result<(), TransposeError> {
+pub fn transpose<const N: usize>(matrix: &mut [u8], rows: usize) -> Result<(), TransposeError>
+where
+    LaneCount<N>: SupportedLaneCount,
+{
     // Check that number of rows is a power of 2
     if rows & (rows - 1) != 0 {
         return Err(TransposeError::InvalidNumberOfRows);
     }
 
-    // Calculate number of rounds for transpose_bytes
-    let rounds = rows.trailing_zeros();
-
     // Check that slice is rectangular
-    if matrix.len() & ((1 << rounds) - 1) != 0 {
+    if matrix.len() & (rows - 1) != 0 {
         return Err(TransposeError::MalformedSlice);
     }
 
+    // Check that row length is multiple of Simd lane
+    if matrix.len() & (N - 1) != 0 {
+        return Err(TransposeError::InvalidLaneCount);
+    }
+
+    // This leads to errors due to the implementation of interleave
+    if N == 1 {
+        return Err(TransposeError::LaneCountOne);
+    }
+
+    // Normal transposition of byte elements
+    // Has to be invoked for ld(rows) rounds
     unsafe {
-        transpose_bytes::<32>(matrix, rounds);
+        transpose_bytes::<N>(matrix, rows.trailing_zeros());
     }
     Ok(())
 }
@@ -36,8 +48,8 @@ where
     LaneCount<N>: SupportedLaneCount,
 {
     let half = matrix.len() >> 1;
-    let mut matrix_copy: Vec<u8> = Vec::with_capacity(matrix.len());
-    for i in 0_..rounds as usize {
+    let mut matrix_copy = vec![0_u8; matrix.len()];
+    for _ in 0..rounds as usize {
         matrix_copy.copy_from_slice(matrix);
         let mut lanes: Vec<Simd<u8, N>> = matrix_copy
             .as_chunks_unchecked_mut::<N>()
@@ -45,10 +57,10 @@ where
             .map(|lane| Simd::from_array(*lane))
             .collect();
         let (chunk1, chunk2) = lanes.split_at_mut_unchecked(half / N);
-        for (v1, v2) in chunk1.iter_mut().zip(chunk2) {
+        for (k, (v1, v2)) in chunk1.iter_mut().zip(chunk2).enumerate() {
             (*v1, *v2) = v1.interleave(*v2);
-            matrix[2 * i * N..(2 * i + 1) * N].copy_from_slice(&v1.to_array());
-            matrix[(2 * i + 1) * N..(2 + 1) * i * N].copy_from_slice(&v2.to_array());
+            matrix[2 * k * N..(2 * k + 1) * N].copy_from_slice(&v1.to_array());
+            matrix[(2 * k + 1) * N..(k + 1) * 2 * N].copy_from_slice(&v2.to_array());
         }
     }
 }
@@ -59,4 +71,35 @@ pub enum TransposeError {
     InvalidNumberOfRows,
     #[error("Provided slice is not of rectangular shape")]
     MalformedSlice,
+    #[error("Number of elements per row must be multiple of lane count")]
+    InvalidLaneCount,
+    #[error(" A lane count of 1 is not supported.")]
+    LaneCountOne,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::distributions::{Distribution, Standard};
+    use rand::prelude::*;
+
+    fn random_matrix<T>(elements: usize) -> Vec<T>
+    where
+        Standard: Distribution<T>,
+    {
+        let mut rng = thread_rng();
+        (0..elements).map(|_| rng.gen::<T>()).collect()
+    }
+
+    #[test]
+    fn test_transpose_bytes() {
+        let rounds = 6;
+        let mut m: Vec<u8> = random_matrix::<u8>(2_usize.pow(rounds) * 2_usize.pow(rounds));
+        let original = m.clone();
+        unsafe {
+            transpose_bytes::<32>(&mut m, rounds);
+            transpose_bytes::<32>(&mut m, rounds);
+        }
+        assert_eq!(m, original);
+    }
 }
