3afe5c9d4ac1e5efe03db9f07de65f92654a1deb
sinuio <>
2022-08-14 14:36:41 -0700

    helper macro

--- utils-aio/src/lib.rs
+++ utils-aio/src/lib.rs
@@ -1,1 +1,20 @@
 pub mod codec;
+
+#[macro_export]
+/// Convenience macro for reading and unwrapping an expected message from a stream which
+/// implements [`futures::StreamExt`]. The caller function must be fallible and provide [`From<std::io::Error>`].
+macro_rules! expected_msg {
+    ($stream:expr, $expected:path) => {
+        if let Some(msg) = $stream.next().await {
+            match msg? {
+                $expected(m) => Ok(m),
+                msg => Err(msg),
+            }
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::UnexpectedEof,
+                "Stream closed unexpectedly",
+            ))?
+        }
+    };
+}
