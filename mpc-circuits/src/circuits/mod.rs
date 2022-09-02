c5c75f4f8f288c5d5ac8ac36aefe831e3f0d3f8e
sinu <65924192+sinui0@users.noreply.github.com>
2022-09-02 10:34:22 -0700

    nbit xor

--- mpc-circuits/src/circuits/mod.rs
+++ mpc-circuits/src/circuits/mod.rs
@@ -5,6 +5,7 @@ mod nbit_adder;
 mod nbit_inverter;
 mod nbit_subtractor;
 mod nbit_switch;
+mod nbit_xor;
 
 pub use full_adder::full_adder;
 pub use half_adder::half_adder;
@@ -13,6 +14,7 @@ pub use nbit_adder::nbit_adder;
 pub use nbit_inverter::nbit_inverter;
 pub use nbit_subtractor::nbit_subtractor;
 pub use nbit_switch::nbit_switch;
+pub use nbit_xor::nbit_xor;
 
 pub fn test_circ(circ: &crate::Circuit, inputs: &[crate::Value], expected: &[crate::Value]) {
     let inputs: Vec<crate::InputValue> = inputs
---
+++ mpc-circuits/src/circuits/nbit_xor.rs
@@ -0,0 +1,63 @@
+use crate::{
+    builder::{CircuitBuilder, Feed, WireHandle},
+    circuit::GateType,
+    Circuit, ValueType,
+};
+
+/// Builds a circuit which computes XOR of two n-bit inputs
+pub fn nbit_xor(n: usize) -> Circuit {
+    let mut builder = CircuitBuilder::new(&format!("{}-bit XOR", n), "0.1.0");
+
+    let a = builder.add_input("A", &format!("{}-bit input", n), ValueType::Bits, n);
+    let b = builder.add_input("B", &format!("{}-bit input", n), ValueType::Bits, n);
+
+    let mut builder = builder.build_inputs();
+
+    let gate_outs: Vec<WireHandle<Feed>> = (0..n)
+        .map(|i| {
+            let xor = builder.add_gate(GateType::Xor);
+            builder.connect(&[a[i], b[i]], &[xor.x(), xor.y().unwrap()]);
+            xor.z()
+        })
+        .collect();
+
+    let mut builder = builder.build_gates();
+    let out = builder.add_output("OUT", &format!("{}-bit output", n), ValueType::Bits, n);
+
+    builder.connect(&gate_outs, &out[..]);
+
+    builder
+        .build_circuit()
+        .expect("failed to build n-bit switch")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{circuits::test_circ, Value};
+
+    #[test]
+    fn test_nbit_xor() {
+        let circ = nbit_xor(8);
+        test_circ(
+            &circ,
+            &[Value::Bits(vec![false; 8]), Value::Bits(vec![true; 8])],
+            &[Value::Bits(vec![true; 8])],
+        );
+        test_circ(
+            &circ,
+            &[Value::Bits(vec![true; 8]), Value::Bits(vec![false; 8])],
+            &[Value::Bits(vec![true; 8])],
+        );
+        test_circ(
+            &circ,
+            &[Value::Bits(vec![false; 8]), Value::Bits(vec![false; 8])],
+            &[Value::Bits(vec![false; 8])],
+        );
+        test_circ(
+            &circ,
+            &[Value::Bits(vec![true; 8]), Value::Bits(vec![true; 8])],
+            &[Value::Bits(vec![false; 8])],
+        );
+    }
+}
