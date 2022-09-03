2fc64a6e39e830399af1e429332b323f545655ed
sinu <65924192+sinui0@users.noreply.github.com>
2022-09-03 15:09:17 -0700

    fix bench

--- tls-2pc-core/benches/garble.rs
+++ tls-2pc-core/benches/garble.rs
@@ -5,14 +5,28 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use mpc_circuits::Circuit;
 use mpc_core::garble::{GarbledCircuit, InputLabels};
 use rand::thread_rng;
-use tls_2pc_core::{CIRCUIT_1, CIRCUIT_2, CIRCUIT_3, CIRCUIT_4, CIRCUIT_5, CIRCUIT_6, CIRCUIT_7};
+
+static CIRCUITS: &[&[u8]] = &[
+    #[cfg(feature = "c1")]
+    tls_2pc_core::CIRCUIT_1,
+    #[cfg(feature = "c2")]
+    tls_2pc_core::CIRCUIT_2,
+    #[cfg(feature = "c3")]
+    tls_2pc_core::CIRCUIT_3,
+    #[cfg(feature = "c4")]
+    tls_2pc_core::CIRCUIT_4,
+    #[cfg(feature = "c5")]
+    tls_2pc_core::CIRCUIT_5,
+    #[cfg(feature = "c6")]
+    tls_2pc_core::CIRCUIT_6,
+    #[cfg(feature = "c7")]
+    tls_2pc_core::CIRCUIT_7,
+];
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("garble_circuits");
 
-    for circ in [
-        CIRCUIT_1, CIRCUIT_2, CIRCUIT_3, CIRCUIT_4, CIRCUIT_5, CIRCUIT_6, CIRCUIT_7,
-    ] {
+    for circ in CIRCUITS {
         let circ = Arc::new(Circuit::load_bytes(circ).unwrap());
         group.bench_function(circ.name(), |b| {
             let mut rng = thread_rng();
