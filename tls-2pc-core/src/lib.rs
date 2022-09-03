d72c49cb6b25c05cc41fc026c892260eefa809c4
sinu <65924192+sinui0@users.noreply.github.com>
2022-09-03 14:53:13 -0700

    redo build script

--- tls-2pc-core/src/lib.rs
+++ tls-2pc-core/src/lib.rs
@@ -5,16 +5,16 @@ pub mod handshake;
 pub mod msgs;
 
 #[cfg(feature = "c1")]
-pub static CIRCUIT_1: &'static [u8] = std::include_bytes!("../circuits/c1.bin");
+pub static CIRCUIT_1: &'static [u8] = std::include_bytes!("../circuits/bin/c1.bin");
 #[cfg(feature = "c2")]
-pub static CIRCUIT_2: &'static [u8] = std::include_bytes!("../circuits/c2.bin");
+pub static CIRCUIT_2: &'static [u8] = std::include_bytes!("../circuits/bin/c2.bin");
 #[cfg(feature = "c3")]
-pub static CIRCUIT_3: &'static [u8] = std::include_bytes!("../circuits/c3.bin");
+pub static CIRCUIT_3: &'static [u8] = std::include_bytes!("../circuits/bin/c3.bin");
 #[cfg(feature = "c4")]
-pub static CIRCUIT_4: &'static [u8] = std::include_bytes!("../circuits/c4.bin");
+pub static CIRCUIT_4: &'static [u8] = std::include_bytes!("../circuits/bin/c4.bin");
 #[cfg(feature = "c5")]
-pub static CIRCUIT_5: &'static [u8] = std::include_bytes!("../circuits/c5.bin");
+pub static CIRCUIT_5: &'static [u8] = std::include_bytes!("../circuits/bin/c5.bin");
 #[cfg(feature = "c6")]
-pub static CIRCUIT_6: &'static [u8] = std::include_bytes!("../circuits/c6.bin");
+pub static CIRCUIT_6: &'static [u8] = std::include_bytes!("../circuits/bin/c6.bin");
 #[cfg(feature = "c7")]
-pub static CIRCUIT_7: &'static [u8] = std::include_bytes!("../circuits/c7.bin");
+pub static CIRCUIT_7: &'static [u8] = std::include_bytes!("../circuits/bin/c7.bin");
