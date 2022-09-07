b68ca78b36221ded3da1872d7b54f0de5ec58e59
sinu <65924192+sinui0@users.noreply.github.com>
2022-09-07 15:05:19 -0700

    c5

--- tls-2pc-core/build.rs
+++ tls-2pc-core/build.rs
@@ -2,9 +2,10 @@ use mpc_circuits::{proto, Circuit};
 use prost::Message;
 use rayon::prelude::*;
 use std::{env, fs, io, path::Path};
-use tls_circuits::{c1, c2, c3, c4};
+use tls_circuits::{c1, c2, c3, c4, c5};
 
-static CIRCUITS: &[(&str, fn() -> Circuit)] = &[("c1", c1), ("c2", c2), ("c3", c3), ("c4", c4)];
+static CIRCUITS: &[(&str, fn() -> Circuit)] =
+    &[("c1", c1), ("c2", c2), ("c3", c3), ("c4", c4), ("c5", c5)];
 
 fn build_circuit<F>(name: &str, f: F) -> io::Result<()>
 where
--- tls-circuits/src/lib.rs
+++ tls-circuits/src/lib.rs
@@ -2,12 +2,14 @@ mod c1;
 mod c2;
 mod c3;
 mod c4;
+mod c5;
 mod combine_pms_shares;
 
 pub use c1::c1;
 pub use c2::c2;
 pub use c3::c3;
 pub use c4::c4;
+pub use c5::c5;
 pub use combine_pms_shares::combine_pms_shares;
 
 static SHA256_STATE: [u32; 8] = [
