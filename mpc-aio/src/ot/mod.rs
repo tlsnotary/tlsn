2eecc11bf48b786a668d46103ec42eac13fe2a8f
th4s <th4s@metavoid.xyz>
2022-09-08 16:25:32 +0200

    WIP: added draft

--- mpc-aio/src/ot/mod.rs
+++ mpc-aio/src/ot/mod.rs
@@ -5,42 +5,58 @@ pub mod extension;
 use async_trait::async_trait;
 pub use errors::OTError;
 use futures::{Sink, Stream};
+use mpc_core::{
+    msgs::ot::OTMessage,
+    ot::{s_state, ExtSenderCoreError, Kos15Sender},
+};
+
+trait Protocol {
+    type Message;
+    type Error;
+}
+
+pub struct Agent<T>
+where
+    T: Protocol,
+{
+    inner: T,
+    stream: Box<dyn Stream<Item = <T as Protocol>::Message> + Unpin + Send>,
+    sink: Box<dyn Sink<<T as Protocol>::Message, Error = T::Error> + Unpin + Send>,
+}
+
+impl<T> Protocol for Kos15Sender<T> {
+    type Message = OTMessage;
+    type Error = ExtSenderCoreError;
+}
+
+impl Agent<Kos15Sender<s_state::Initialized>> {
+    fn setup(self) -> Kos15Sender<s_state::Setup> {
+        todo!()
+    }
+}
 
 #[async_trait]
 pub trait ObliviousSend {
     type Inputs;
     type Envelope;
-    type Message;
 
     async fn send(
         &mut self,
-        stream: impl Stream<Item = Self::Message> + Unpin + Send,
-        sink: impl Sink<Self::Message> + Unpin + Send,
         inputs: Self::Inputs,
     ) -> Box<dyn Stream<Item = Result<Self::Envelope, OTError>>>;
+}
+
+impl ObliviousSend for Agent<Kos15Sender<s_state::Setup>> {
+    // ...
 }
 
 #[async_trait]
 pub trait ObliviousReceive {
     type Choices;
     type Outputs;
-    type Message;
 
     async fn receive(
         &mut self,
-        stream: impl Stream<Item = Self::Message> + Unpin + Send,
-        sink: impl Sink<Self::Message> + Unpin + Send,
         choices: Self::Choices,
     ) -> Box<dyn Stream<Item = Result<Self::Outputs, OTError>>>;
-}
-
-#[async_trait]
-pub trait ObliviousSetup {
-    type Actor;
-    type Message;
-
-    async fn setup(
-        stream: impl Stream<Item = Self::Message> + Unpin + Send,
-        sink: impl Sink<Self::Message> + Unpin + Send,
-    ) -> Result<Self::Actor, OTError>;
 }
