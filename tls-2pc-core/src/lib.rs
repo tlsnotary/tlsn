2ae0c64bdcd4874bffb0fb6db66b3a17649dc458
sinu <65924192+sinui0@users.noreply.github.com>
2022-09-21 13:02:22 -0700

    session key shares

--- tls-2pc-core/src/lib.rs
+++ tls-2pc-core/src/lib.rs
@@ -20,3 +20,37 @@ pub static CIRCUIT_5: &'static [u8] = std::include_bytes!("../circuits/bin/c5.bi
 pub static CIRCUIT_6: &'static [u8] = std::include_bytes!("../circuits/bin/c6.bin");
 #[cfg(feature = "c7")]
 pub static CIRCUIT_7: &'static [u8] = std::include_bytes!("../circuits/bin/c7.bin");
+
+pub struct SessionKeyShares {
+    swk: [u8; 16],
+    cwk: [u8; 16],
+    siv: [u8; 4],
+    civ: [u8; 4],
+}
+
+impl SessionKeyShares {
+    /// Creates new SessionKeyShares
+    pub fn new(swk: [u8; 16], cwk: [u8; 16], siv: [u8; 4], civ: [u8; 4]) -> Self {
+        Self { swk, cwk, siv, civ }
+    }
+
+    /// Returns server_write_key share
+    pub fn swk(&self) -> [u8; 16] {
+        self.swk
+    }
+
+    /// Returns client_write_key share
+    pub fn cwk(&self) -> [u8; 16] {
+        self.cwk
+    }
+
+    /// Returns server IV share
+    pub fn siv(&self) -> [u8; 4] {
+        self.siv
+    }
+
+    /// Returns client IV share
+    pub fn civ(&self) -> [u8; 4] {
+        self.civ
+    }
+}
