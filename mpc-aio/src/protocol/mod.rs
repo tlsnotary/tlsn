8404e481f76c4700455d9e309944bdd485ceb3c6
sinu.eth <65924192+sinui0@users.noreply.github.com>
2022-09-26 09:50:50 -0700

    Refactor point addition in mpc-aio (#85)

--- mpc-aio/src/protocol/mod.rs
+++ mpc-aio/src/protocol/mod.rs
@@ -1,8 +1,8 @@
+pub mod garble;
 #[cfg(feature = "ot")]
 pub mod ot;
-//#[cfg(feature = "pa")]
-//pub mod point_addition;
-pub mod garble;
+#[cfg(feature = "pa")]
+pub mod point_addition;
 
 pub trait Protocol {
     type Message: Send + 'static;
--- mpc-aio/src/protocol/point_addition/mod.rs
+++ mpc-aio/src/protocol/point_addition/mod.rs
@@ -1,12 +1,26 @@
-pub mod errors;
-pub mod follower;
-pub mod leader;
+mod follower;
+mod leader;
 
-pub use errors::PointAdditionError;
 pub use follower::PaillierFollower;
 pub use leader::PaillierLeader;
 
-use mpc_core::point_addition::SecretShare;
+use mpc_core::{
+    msgs::point_addition::PointAdditionMessage,
+    point_addition::{P256SecretShare, PointAdditionError as CoreError},
+};
+use utils_aio::Channel;
+
+pub type PAChannel = Box<dyn Channel<PointAdditionMessage, Error = std::io::Error>>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum PointAdditionError {
+    #[error("Secret share failed due to io error: {0}")]
+    IOError(#[from] std::io::Error),
+    #[error("Encountered core error: {0:?}")]
+    CoreError(#[from] CoreError),
+    #[error("Unexpected message")]
+    UnexpectedMessage(PointAdditionMessage),
+}
 
 use async_trait::async_trait;
 use mockall::automock;
@@ -16,49 +30,55 @@ use mockall::automock;
 #[automock]
 #[async_trait]
 pub trait PointAddition2PC {
-    async fn add(&mut self, point: &p256::EncodedPoint) -> Result<SecretShare, PointAdditionError>;
+    async fn add(
+        &mut self,
+        point: &p256::EncodedPoint,
+    ) -> Result<P256SecretShare, PointAdditionError>;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use mpc_core::point_addition::combine_shares;
     use p256::{elliptic_curve::sec1::ToEncodedPoint, SecretKey};
     use rand::thread_rng;
+    use utils_aio::duplex::DuplexChannel;
 
     #[tokio::test]
     async fn test_point_addition() {
-        let (socket_m, socket_s) = tokio::net::UnixStream::pair().unwrap();
+        let (leader_channel, follower_channel) = DuplexChannel::new();
 
-        let mut master = PaillierLeader::new(socket_m);
-        let mut slave = PaillierFollower::new(socket_s);
+        let mut leader = PaillierLeader::new(Box::new(leader_channel));
+        let mut follower = PaillierFollower::new(Box::new(follower_channel));
 
         let mut rng = thread_rng();
 
         let server_secret = SecretKey::random(&mut rng);
         let server_pk = server_secret.public_key().to_projective();
 
-        let master_secret = SecretKey::random(&mut rng);
-        let master_point =
-            (&server_pk * &master_secret.to_nonzero_scalar()).to_encoded_point(false);
+        let leader_secret = SecretKey::random(&mut rng);
+        let leader_point =
+            (&server_pk * &leader_secret.to_nonzero_scalar()).to_encoded_point(false);
 
-        let slave_secret = SecretKey::random(&mut rng);
-        let slave_point = (&server_pk * &slave_secret.to_nonzero_scalar()).to_encoded_point(false);
+        let follower_secret = SecretKey::random(&mut rng);
+        let follower_point =
+            (&server_pk * &follower_secret.to_nonzero_scalar()).to_encoded_point(false);
 
         let (task_m, task_s) = tokio::join!(
-            tokio::spawn(async move { master.add(&master_point).await }),
-            tokio::spawn(async move { slave.add(&slave_point).await })
+            tokio::spawn(async move { leader.add(&leader_point).await }),
+            tokio::spawn(async move { follower.add(&follower_point).await })
         );
 
-        let master_share = task_m.unwrap().unwrap();
-        let slave_share = task_s.unwrap().unwrap();
+        let leader_share = task_m.unwrap().unwrap();
+        let follower_share = task_s.unwrap().unwrap();
 
-        let pms = ((&server_pk * &master_secret.to_nonzero_scalar())
-            + (&server_pk * &slave_secret.to_nonzero_scalar()))
-            .to_affine();
+        let pms = ((&server_pk * &leader_secret.to_nonzero_scalar())
+            + (&server_pk * &follower_secret.to_nonzero_scalar()))
+            .to_affine()
+            .to_encoded_point(false)
+            .x()
+            .unwrap()
+            .to_vec();
 
-        let pms = *pms.to_encoded_point(false).x().unwrap();
-
-        assert_eq!(pms.to_vec(), combine_shares(master_share, slave_share));
+        assert_eq!(pms.to_vec(), leader_share + follower_share);
     }
 }
