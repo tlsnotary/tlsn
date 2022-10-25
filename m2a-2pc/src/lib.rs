36ff94641eac38ef37c13cafb9d78f7048b4965a
th4s <th4s@metavoid.xyz>
2022-10-25 11:41:45 +0200

    Renamed subcrate and
    - bugfix in `receive` method
    - improved doc
    - added struct for messaging

--- m2a-2pc/src/lib.rs
+++ m2a-2pc/src/lib.rs
@@ -1,12 +1,12 @@
-//! This subcrate implements a secure two-party (2PC) multiplication algorithm
+//! This subcrate implements a secure two-party (2PC) multiplication-to-addition (M2A) algorithm
 //!
 //! Let `A` be an element of some finite field with `A = a * b`, where `a` is only known to Alice
 //! and `b` is only known to Bob. A is unknown to both parties and it is their goal that each of
 //! them ends up with an additive share of A. So both parties start with `a` and `b` and want to
 //! end up with `x` and `y`, where `A = a * b = x + y`.
 //!
-//! This is an implementation of the oblivious transfer method in chapter 4.1 of
-//! <https://link.springer.com/content/pdf/10.1007/3-540-48405-1_8.pdf>
+//! This is an implementation for the extension field GF(2^128) of the oblivious transfer method in
+//! chapter 4.1 of <https://link.springer.com/content/pdf/10.1007/3-540-48405-1_8.pdf>
 
 mod receiver;
 mod sender;
@@ -27,9 +27,37 @@ fn mul_gf2_128(mut x: u128, y: u128) -> u128 {
     result
 }
 
+/// The message between `Sender` and `Receiver`
+pub struct M2AChoices([u128; 128], [u128; 128]);
+
 /// Error for 2PC mulitplication
 #[derive(Debug, Error)]
-pub enum Mul2PCError {
+pub enum M2AError {
     #[error("Choices are still missing")]
     ChoicesMissing,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::{Rng, SeedableRng};
+    use rand_chacha::ChaCha12Rng;
+
+    #[test]
+    fn test_mul2pc() {
+        let mut rng = ChaCha12Rng::from_entropy();
+        let a: u128 = rng.gen();
+        let b: u128 = rng.gen();
+
+        let mut receiver = Receiver::new(a);
+        let sender = Sender::new(b);
+
+        let choices = sender.send();
+        receiver.receive(choices);
+
+        assert_eq!(
+            mul_gf2_128(a, b),
+            sender.finalize() ^ receiver.finalize().unwrap()
+        );
+    }
 }
