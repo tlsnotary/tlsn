8d89b7068fc5f849aba85907b121c0706c30ddc9
th4s <th4s@metavoid.xyz>
2022-11-04 16:31:55 +0100

    WIP: Added receiver side

--- tls-2pc-core/src/ghash_new/receiver.rs
+++ tls-2pc-core/src/ghash_new/receiver.rs
@@ -1,1 +1,92 @@
-pub struct GhashReceiver {}
+use super::{compute_powers, mul, AddShare, MulShare};
+
+/// The receiver part for our 2PC Ghash implementation
+///
+/// `GhashReceiver` will be the receiver side during the oblivious transfer.
+pub struct GhashReceiver<T = AddShare> {
+    /// Different hashkey representations
+    hashkey_repr: T,
+    /// The ciphertext for which a 2PC MAC should be constructed
+    ciphertext: Vec<u128>,
+}
+
+impl GhashReceiver {
+    /// Create a new `GhashReceiver`
+    ///
+    /// * `hashkey` - This is `H`, which is the AES-encrypted 0 block
+    /// * `ciphertext` - The AES-encrypted 128-bit blocks
+    pub fn new(hashkey: u128, ciphertext: Vec<u128>) -> Self {
+        Self {
+            hashkey_repr: AddShare::new(hashkey),
+            ciphertext,
+        }
+    }
+
+    /// Return the choices, needed for the oblivious transfer
+    ///
+    /// The bits of the returned `u128` encode the choices for
+    /// the OT
+    pub fn choices(&self) -> u128 {
+        self.hashkey_repr.inner()
+    }
+
+    /// Transform `self` into a `GhashReceiver` holding multiplicative shares of powers of `H`
+    ///
+    /// Converts the additive share into multiplicative shares of powers of `H`.
+    ///
+    /// * `chosen_inputs` - the result of the oblivious transfer.
+    pub fn compute_mul_powers(self, chosen_inputs: [u128; 128]) -> GhashReceiver<Vec<MulShare>> {
+        let mul_share = MulShare::from_choice(chosen_inputs);
+
+        let hashkey_powers = compute_powers(mul_share.inner(), self.ciphertext.len())
+            .into_iter()
+            .map(|power| MulShare::new(power))
+            .collect();
+
+        GhashReceiver {
+            hashkey_repr: hashkey_powers,
+            ciphertext: self.ciphertext,
+        }
+    }
+}
+
+impl GhashReceiver<Vec<MulShare>> {
+    /// Return the choices, needed for the batched oblivious transfer
+    ///
+    /// The bits of the returned `Vec<u128>` encode the choices for
+    /// the OTs
+    pub fn choices(&self) -> Vec<u128> {
+        self.hashkey_repr.iter().map(|x| x.inner()).collect()
+    }
+
+    /// Convert all powers of `H` into additive shares
+    ///
+    /// Converts the multiplicative shares into additive ones.
+    ///
+    /// * `chosen_inputs` - the results of the batched oblivious transfer.
+    pub fn into_add_powers(self, chosen_inputs: Vec<[u128; 128]>) -> GhashReceiver<Vec<AddShare>> {
+        let hashkey_powers: Vec<AddShare> = chosen_inputs
+            .into_iter()
+            .map(|input| AddShare::from_choice(input))
+            .collect();
+
+        GhashReceiver {
+            hashkey_repr: hashkey_powers,
+            ciphertext: self.ciphertext,
+        }
+    }
+}
+
+impl GhashReceiver<Vec<AddShare>> {
+    /// Compute the final MAC
+    ///
+    /// Computes the 2PC additive share of the MAC of `self.ciphertext`
+    pub fn into_mac(self) -> u128 {
+        self.hashkey_repr
+            .into_iter()
+            .enumerate()
+            .fold(0, |acc, (k, hashkey_power)| {
+                acc ^ mul(hashkey_power.inner(), self.ciphertext[k])
+            })
+    }
+}
