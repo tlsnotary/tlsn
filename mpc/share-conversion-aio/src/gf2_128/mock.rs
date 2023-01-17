a77059c0efc8368f1b0c07b8597a493117a35505
sinu <65924192+sinui0@users.noreply.github.com>
2023-01-17 13:42:20 -0800

    update mock ot factory to have a control and handle instance ids

--- mpc/share-conversion-aio/src/gf2_128/mock.rs
+++ mpc/share-conversion-aio/src/gf2_128/mock.rs
@@ -2,26 +2,19 @@ use super::{recorder::Recorder, Gf2ConversionMessage, Receiver, Sender};
 use mpc_aio::protocol::ot::mock::{MockOTFactory, MockOTReceiver, MockOTSender};
 use mpc_core::Block;
 use share_conversion_core::gf2_128::Gf2_128ShareConvert;
-use std::sync::{Arc, Mutex};
 use utils_aio::duplex::DuplexChannel;
 
 pub type Gf2ConversionChannel = DuplexChannel<Gf2ConversionMessage>;
-pub type Gf2Sender<U, V> = Sender<Arc<Mutex<MockOTFactory<Block>>>, MockOTSender<Block>, U, V>;
-pub type Gf2Receiver<U, V> =
-    Receiver<Arc<Mutex<MockOTFactory<Block>>>, MockOTReceiver<Block>, U, V>;
+pub type Gf2Sender<U, V> = Sender<MockOTFactory<Block>, MockOTSender<Block>, U, V>;
+pub type Gf2Receiver<U, V> = Receiver<MockOTFactory<Block>, MockOTReceiver<Block>, U, V>;
 
 pub fn mock_converter_pair<U: Gf2_128ShareConvert, V: Recorder<U>>(
 ) -> (Gf2Sender<U, V>, Gf2Receiver<U, V>) {
     let (c1, c2): (Gf2ConversionChannel, Gf2ConversionChannel) = DuplexChannel::new();
-    let ot_factory = Arc::new(Mutex::new(MockOTFactory::<Block>::default()));
+    let ot_factory = MockOTFactory::new();
 
-    let sender = Sender::new(
-        Arc::clone(&ot_factory),
-        String::from(""),
-        Box::new(c1),
-        None,
-    );
-    let receiver = Receiver::new(Arc::clone(&ot_factory), String::from(""), Box::new(c2));
+    let sender = Sender::new(ot_factory.clone(), String::from(""), Box::new(c1), None);
+    let receiver = Receiver::new(ot_factory, String::from(""), Box::new(c2));
 
     (sender, receiver)
 }
