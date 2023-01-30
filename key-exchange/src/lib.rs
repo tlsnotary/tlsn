7622dbe1ac2ada7e57a43aca92438f10490948b7
sinu <65924192+sinui0@users.noreply.github.com>
2023-01-29 22:43:27 -0800

    add naive key exchange impl

--- key-exchange/src/lib.rs
+++ key-exchange/src/lib.rs
@@ -1,41 +1,110 @@
-pub mod mock;
+mod follower;
+mod leader;
+mod msg;
+pub mod point_addition;
 
 use async_trait::async_trait;
+use p256::PublicKey;
 
-use tls_core::key::PublicKey;
+use utils_aio::Channel;
+
+pub use follower::KeyExchangeFollower;
+pub use leader::KeyExchangeLeader;
+pub use msg::KeyExchangeMessage;
+pub use point_addition::{PointAddition, PointAdditionError};
+
+pub type KeyExchangeChannel = Box<dyn Channel<KeyExchangeMessage, Error = std::io::Error> + Send>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum KeyExchangeError {
+    #[error("KeyError: {0}")]
+    KeyError(String),
+    #[error("KeyParseError: {0}")]
+    KeyParseError(String),
+    #[error("IOError: {0}")]
+    IOError(#[from] std::io::Error),
+    #[error("UnexpectedMessage: {0:?}")]
+    UnexpectedMessage(KeyExchangeMessage),
     #[error("PointAdditionError: {0}")]
-    PointAdditionError(#[from] PointAdditionError),
+    PointAdditionError(#[from] point_addition::PointAdditionError),
 }
 
 #[async_trait]
 pub trait KeyExchange {
-    /// Sets the public key share.
-    async fn set_key_share(&mut self, key: PublicKey) -> Result<(), KeyExchangeError>;
     /// Returns the client's public key share.
     async fn get_client_key_share(&mut self) -> Result<PublicKey, KeyExchangeError>;
     /// Sets the server's public key share.
     async fn set_server_key_share(&mut self, key: PublicKey) -> Result<(), KeyExchangeError>;
-    /// Computes the pms share.
+
+    /// Computes the PMS share.
+    ///
+    /// Returns the PMS share as a big-endian byte array.
     async fn get_pms_share(&mut self) -> Result<Vec<u8>, KeyExchangeError>;
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum PointAdditionError {}
+pub mod mock {
+    use utils_aio::duplex::DuplexChannel;
 
-/// This trait is for securely secret-sharing the addition of two elliptic curve points.
-/// Let `P + Q = O = (x, y)`. Each party receives additive shares of the x-coordinate.
-#[async_trait]
-pub trait PointAddition {
-    type Point;
-    type XCoordinate;
+    use super::*;
 
-    /// Adds two elliptic curve points in 2PC, returning respective secret shares
-    /// of the resulting x-coordinate to both parties.
-    async fn share_x_coordinate(
-        &mut self,
-        point: Self::Point,
-    ) -> Result<Self::XCoordinate, PointAdditionError>;
+    pub type MockKeyExchangeLeader = KeyExchangeLeader<point_addition::mock::MockP256PointAddition>;
+    pub type MockKeyExchangeFollower =
+        KeyExchangeFollower<point_addition::mock::MockP256PointAddition>;
+
+    pub fn create_mock_key_exchange_pair() -> (MockKeyExchangeLeader, MockKeyExchangeFollower) {
+        let (leader_channel, follower_channel) = DuplexChannel::<KeyExchangeMessage>::new();
+        let point_addition = point_addition::mock::MockP256PointAddition::new();
+
+        let leader = KeyExchangeLeader::new(Box::new(leader_channel), point_addition.clone());
+        let follower = KeyExchangeFollower::new(Box::new(follower_channel), point_addition);
+
+        (leader, follower)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use p256::{
+            elliptic_curve::{generic_array, AffineXCoordinate, PrimeField},
+            Scalar, SecretKey,
+        };
+        use rand::SeedableRng;
+        use rand_chacha::ChaCha12Rng;
+
+        use super::*;
+
+        #[tokio::test]
+        async fn test_key_exchange() {
+            let (mut leader, mut follower) = create_mock_key_exchange_pair();
+
+            let server_secret = SecretKey::random(&mut ChaCha12Rng::seed_from_u64(0));
+            let server_pk = server_secret.public_key();
+
+            leader.set_server_key_share(server_pk).await.unwrap();
+
+            let follower_task =
+                tokio::spawn(async move { follower.get_pms_share().await.unwrap() });
+
+            let client_key_share = leader.get_client_key_share().await.unwrap();
+
+            let leader_pms_share = leader.get_pms_share().await.unwrap();
+            let follower_pms_share = follower_task.await.unwrap();
+
+            let leader_pms_share = generic_array::GenericArray::from_slice(&leader_pms_share);
+            let follower_pms_share = generic_array::GenericArray::from_slice(&follower_pms_share);
+
+            let leader_pms_share = Scalar::from_repr(*leader_pms_share).unwrap();
+            let follower_pms_share = Scalar::from_repr(*follower_pms_share).unwrap();
+
+            let pms = leader_pms_share + follower_pms_share;
+
+            let expected_pms = Scalar::from_repr(
+                (&client_key_share.to_projective() * &server_secret.to_nonzero_scalar())
+                    .to_affine()
+                    .x(),
+            )
+            .unwrap();
+
+            assert_eq!(pms, expected_pms);
+        }
+    }
 }
