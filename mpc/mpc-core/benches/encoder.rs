8bad50199cf02976c73706e13d77457d265237d4
sinu <65924192+sinui0@users.noreply.github.com>
2023-02-19 18:40:06 -0800

    update encoder

--- mpc/mpc-core/benches/encoder.rs
+++ mpc/mpc-core/benches/encoder.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use mpc_circuits::{Circuit, WireGroup, AES_128_REVERSE};
+use mpc_circuits::{BitOrder, Circuit, WireGroup, AES_128_REVERSE};
 use mpc_core::garble::{ChaChaEncoder, Encoder};
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -9,12 +9,12 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     let circ = Arc::new(Circuit::load_bytes(AES_128_REVERSE).unwrap());
     group.bench_function(circ.id().clone().to_string(), |b| {
-        let mut enc = ChaChaEncoder::new([0u8; 32]);
+        let mut enc = ChaChaEncoder::new([0u8; 32], BitOrder::Msb0);
         b.iter(|| {
             black_box(
                 circ.inputs()
                     .iter()
-                    .map(|input| enc.encode(input.index() as u32, input, false))
+                    .map(|input| enc.encode(input.index() as u32, input))
                     .collect::<Vec<_>>(),
             )
         })
