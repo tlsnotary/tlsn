7b67999212c39e33f96c0d4a3f29864df60dfac8
th4s <th4s@metavoid.xyz>
2023-03-01 12:20:41 +0100

    Sealed role trait

--- key-exchange/src/role.rs
+++ key-exchange/src/role.rs
@@ -1,7 +1,7 @@
 //! This module helps defining the role for the instance used in the key exchange protocol
 
 /// A trait which depending on the Role allows to get the correct input numbers for the circuit
-pub trait Role {
+pub trait Role: sealed::Sealed {
     /// Get the correct input number for the first input
     fn first_input(&self) -> usize;
     /// Get the correct input number for the second input
@@ -54,4 +54,13 @@ impl Role for Follower {
     fn fourth_input(&self) -> usize {
         2
     }
+}
+
+mod sealed {
+    use super::*;
+
+    pub trait Sealed {}
+
+    impl Sealed for Leader {}
+    impl Sealed for Follower {}
 }
