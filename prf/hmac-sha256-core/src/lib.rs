bc176c98d61b2a8ac1e1815fd24f2a94f6ce2586
sinu <65924192+sinui0@users.noreply.github.com>
2023-03-20 16:28:29 -0700

    PR feedback

--- prf/hmac-sha256-core/src/lib.rs
+++ prf/hmac-sha256-core/src/lib.rs
@@ -11,15 +11,78 @@ use mpc_core::garble::{ActiveLabels, FullLabels};
 use once_cell::sync::Lazy;
 use std::sync::Arc;
 
+/// Master secret
+///
+/// Computes the master secret (MS), returning the outer and inner HMAC states.
+///
+/// Outer state is H(master_secret ⊕ opad)
+///
+/// Inner state is H(master_secret ⊕ ipad)
+///
+/// Inputs:
+///
+///   0. PMS: 32-byte pre-master secret
+///   1. CLIENT_RAND: 32-byte client random
+///   2. SERVER_RAND: 32-byte server random
+///
+/// Outputs:
+///
+///   0. OUTER_STATE: 32-byte HMAC outer hash state
+///   1. INNER_STATE: 32-byte HMAC inner hash state
 pub static MS: Lazy<Arc<Circuit>> = Lazy::new(|| {
     Circuit::load_bytes(std::include_bytes!("../circuits/bin/master_secret.bin")).unwrap()
 });
+
+/// Session Keys
+///
+/// Compute expanded p1 which consists of client_write_key + server_write_key
+/// Compute expanded p2 which consists of client_IV + server_IV
+///
+/// Inputs:
+///
+///   0. OUTER_HASH_STATE: 32-byte MS outer-hash state
+///   1. INNER_HASH_STATE: 32-byte MS inner-hash state
+///   2. CLIENT_RAND: 32-byte client random
+///   3. SERVER_RAND: 32-byte server random
+///
+/// Outputs:
+///
+///   0. CWK: 16-byte client write-key
+///   1. SWK: 16-byte server write-key
+///   2. CIV: 4-byte client IV
+///   3. SIV: 4-byte server IV
 pub static SESSION_KEYS: Lazy<Arc<Circuit>> = Lazy::new(|| {
     Circuit::load_bytes(std::include_bytes!("../circuits/bin/session_keys.bin")).unwrap()
 });
+
+/// Computes client finished verify_data as specified in RFC 5246, Section 7.4.9.
+///
+/// Inputs:
+///
+///   0. OUTER_STATE: 32-byte MS outer-hash state H(ms ⊕ opad)
+///   1. INNER_STATE: 32-byte MS inner-hash state H(ms ⊕ ipad)
+///   2. HS_HASH: 32-byte handshake hash
+///   3. MASK: 12-byte mask for verify_data
+///
+/// Outputs:
+///
+///   0. MASKED_VD: 12-byte masked client finished verify_data (VD + MASK)
 pub static CF_VD: Lazy<Arc<Circuit>> = Lazy::new(|| {
     Circuit::load_bytes(std::include_bytes!("../circuits/bin/cf_verify_data.bin")).unwrap()
 });
+
+/// Computes server finished verify_data as specified in RFC 5246, Section 7.4.9.
+///
+/// Inputs:
+///
+///   0. OUTER_STATE: 32-byte MS outer-hash state H(ms ⊕ opad)
+///   1. INNER_STATE: 32-byte MS inner-hash state H(ms ⊕ ipad)
+///   2. HS_HASH: 32-byte handshake hash
+///   3. MASK: 12-byte mask for verify_data
+///
+/// Outputs:
+///
+///   0. MASKED_VD: 12-byte masked server finished verify_data (VD + MASK)
 pub static SF_VD: Lazy<Arc<Circuit>> = Lazy::new(|| {
     Circuit::load_bytes(std::include_bytes!("../circuits/bin/sf_verify_data.bin")).unwrap()
 });
--- prf/hmac-sha256-utils/src/lib.rs
+++ prf/hmac-sha256-utils/src/lib.rs
@@ -15,7 +15,7 @@ pub static SHA256_INITIAL_STATE: [u32; 8] = [
 ];
 
 pub fn partial_sha256_digest(input: &[u8]) -> [u32; 8] {
-    let mut state = SHA256_INITIAL_STATE.clone();
+    let mut state = SHA256_INITIAL_STATE;
     for b in input.chunks(64) {
         let mut block = [0u8; 64];
         block[..b.len()].copy_from_slice(b);
@@ -89,7 +89,10 @@ pub fn prf(key: &[u8], label: &[u8], seed: &[u8], bytes: usize) -> Vec<u8> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use hmac::{Hmac, Mac};
     use sha2::{Digest, Sha256};
+
+    type HmacSha256 = Hmac<Sha256>;
 
     #[test]
     fn test_sha2_initial_state() {
@@ -121,5 +124,97 @@ mod tests {
         hasher.update(s);
         hasher.update(s2);
         assert_eq!(digest, hasher.finalize().as_slice());
+    }
+
+    #[test]
+    fn test_partial_hmac() {
+        let key = [42u8; 32];
+        let msg = b"test string";
+        let (outer_state, inner_state) = partial_hmac(&key);
+
+        let hmac = finalize_sha256_digest(
+            outer_state,
+            64,
+            finalize_sha256_digest(inner_state, 64, msg).as_slice(),
+        );
+
+        let expected_hmac: [u8; 32] = {
+            let mut hmac = HmacSha256::new_from_slice(&key).unwrap();
+            hmac.update(msg);
+            hmac.finalize().into_bytes().into()
+        };
+
+        assert_eq!(hmac, expected_hmac);
+    }
+
+    #[test]
+    fn test_hmac() {
+        let key = [42u8; 32];
+        let msg = b"test string";
+
+        let hmac = hmac(&key, msg);
+
+        let expected_hmac = {
+            let mut hmac = HmacSha256::new_from_slice(&key).unwrap();
+            hmac.update(msg);
+            hmac.finalize().into_bytes().to_vec()
+        };
+
+        assert_eq!(hmac, expected_hmac);
+    }
+
+    #[test]
+    fn test_prf() {
+        let key = [42u8; 32];
+        let seed = [69u8; 64];
+        let label = b"test label";
+
+        let output = prf(&key, label, &seed, 32);
+
+        let mut expected_output = [0u8; 32];
+        ring_prf::prf(&mut expected_output, &key, label, &seed);
+
+        assert_eq!(output, expected_output);
+    }
+
+    // Borrowed from Rustls for testing (we don't want ring as a dependency)
+    // https://github.com/rustls/rustls/blob/main/rustls/src/tls12/prf.rs
+    mod ring_prf {
+        use ring::{hmac, hmac::HMAC_SHA256};
+
+        fn concat_sign(key: &hmac::Key, a: &[u8], b: &[u8]) -> hmac::Tag {
+            let mut ctx = hmac::Context::with_key(key);
+            ctx.update(a);
+            ctx.update(b);
+            ctx.sign()
+        }
+
+        fn p(out: &mut [u8], secret: &[u8], seed: &[u8]) {
+            let hmac_key = hmac::Key::new(HMAC_SHA256, secret);
+
+            // A(1)
+            let mut current_a = hmac::sign(&hmac_key, seed);
+            let chunk_size = HMAC_SHA256.digest_algorithm().output_len;
+            for chunk in out.chunks_mut(chunk_size) {
+                // P_hash[i] = HMAC_hash(secret, A(i) + seed)
+                let p_term = concat_sign(&hmac_key, current_a.as_ref(), seed);
+                chunk.copy_from_slice(&p_term.as_ref()[..chunk.len()]);
+
+                // A(i+1) = HMAC_hash(secret, A(i))
+                current_a = hmac::sign(&hmac_key, current_a.as_ref());
+            }
+        }
+
+        fn concat(a: &[u8], b: &[u8]) -> Vec<u8> {
+            let mut ret = Vec::new();
+            ret.extend_from_slice(a);
+            ret.extend_from_slice(b);
+            ret
+        }
+
+        pub fn prf(out: &mut [u8], secret: &[u8], label: &[u8], seed: &[u8]) {
+            let joined_seed = concat(label, seed);
+            p(out, secret, &joined_seed);
+        }
     }
 }
