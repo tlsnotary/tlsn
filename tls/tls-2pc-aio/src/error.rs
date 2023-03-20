3efc914e3022bbcb97ab509099dde681bb3d1536
sinu <65924192+sinui0@users.noreply.github.com>
2023-03-20 16:30:31 -0700

    remove deprecated crates

--- tls/tls-2pc-aio/src/error.rs
+++
@@ -1,9 +0,0 @@
-#[derive(thiserror::Error, Debug, Clone, PartialEq)]
-pub enum Error {
-    #[error("Encountered error with ClientConnection: {0:?}")]
-    ClientConnectionError(#[from] tls_client::Error),
-    #[error("Encountered error during encryption")]
-    EncryptError,
-    #[error("Encountered error during decryption")]
-    DecryptError,
-}
