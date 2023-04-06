1d774b1e31a0d241b0ca3c56f80684a200781483
th4s <th4s@metavoid.xyz>
2023-04-06 12:09:44 +0200

    Removed OT trait implementations for GC from ot crates

--- mpc/ot/mpc-ot/src/lib.rs
+++ mpc/ot/mpc-ot/src/lib.rs
@@ -4,9 +4,6 @@ pub mod mock;
 
 use async_trait::async_trait;
 use futures::channel::oneshot::Canceled;
-use mpc_circuits::{InputValue, WireGroup};
-use mpc_core::Block;
-use mpc_garble_core::{ActiveEncodedInput, ActiveLabels, FullEncodedInput};
 use mpc_ot_core::{
     msgs::{OTFactoryMessage, OTMessage},
     CommittedOTError, ExtReceiverCoreError, ExtSenderCoreError, ReceiverCoreError, SenderCoreError,
@@ -89,74 +86,11 @@ pub trait ObliviousVerify<T> {
     async fn verify(self, input: Vec<T>) -> Result<(), OTError>;
 }
 
-#[async_trait]
-impl<T> ObliviousSend<FullEncodedInput> for T
-where
-    T: Send + ObliviousSend<[Block; 2]>,
-{
-    async fn send(&mut self, inputs: Vec<FullEncodedInput>) -> Result<(), OTError> {
-        self.send(
-            inputs
-                .into_iter()
-                .map(|labels| labels.iter_blocks().collect::<Vec<[Block; 2]>>())
-                .flatten()
-                .collect::<Vec<[Block; 2]>>(),
-        )
-        .await
-    }
-}
-
-#[async_trait]
-impl<T> ObliviousReceive<InputValue, ActiveEncodedInput> for T
-where
-    T: Send + ObliviousReceive<bool, Block>,
-{
-    async fn receive(
-        &mut self,
-        choices: Vec<InputValue>,
-    ) -> Result<Vec<ActiveEncodedInput>, OTError> {
-        let choice_bits = choices
-            .iter()
-            .map(|value| value.value().to_bits(value.bit_order()))
-            .flatten()
-            .collect::<Vec<bool>>();
-
-        let mut blocks = self.receive(choice_bits).await?;
-
-        Ok(choices
-            .into_iter()
-            .map(|value| {
-                let labels = ActiveLabels::from_blocks(blocks.drain(..value.len()).collect());
-                ActiveEncodedInput::from_active_labels(value.group().clone(), labels)
-                    .expect("Input labels should be valid")
-            })
-            .collect())
-    }
-}
-
-#[async_trait]
-impl<T> ObliviousVerify<FullEncodedInput> for T
-where
-    T: Send + ObliviousVerify<[Block; 2]>,
-{
-    async fn verify(self, input: Vec<FullEncodedInput>) -> Result<(), OTError> {
-        self.verify(
-            input
-                .into_iter()
-                .map(|labels| labels.iter_blocks().collect::<Vec<[Block; 2]>>())
-                .flatten()
-                .collect(),
-        )
-        .await
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::mock::mock_ot_pair;
-    use mpc_circuits::ADDER_64;
-    use mpc_garble_core::FullInputSet;
+    use mpc_circuits::{InputValue, WireGroup, ADDER_64};
+    use mpc_garble_core::{ActiveEncodedInput, ActiveLabels, FullEncodedInput, FullInputSet};
     use rand::thread_rng;
 
     #[tokio::test]
