ef4fd3432c0bc5684489180062701c2e2a333ce0
sinu <65924192+sinui0@users.noreply.github.com>
2023-05-22 17:16:58 -0700

    fix mpc crate

--- mpc/garble/mpc-garble/src/protocol/deap/vm.rs
+++ mpc/garble/mpc-garble/src/protocol/deap/vm.rs
@@ -127,7 +127,7 @@ where
             return Err(VmError::ThreadAlreadyExists(thread_id.to_string()));
         }
 
-        let channel = self.channel_factory.get_channel(thread_id).await?;
+        let channel = self.channel_factory.get_channel(&thread_id.to_string()).await?;
 
         Ok(DEAPThread::new(
             thread_id,
