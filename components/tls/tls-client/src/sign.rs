b98a01ffb4283a43aa3fd6c5e7a7c2520b41468e
sinu <65924192+sinui0@users.noreply.github.com>
2023-08-07 16:24:17 -0700

    remove ring from sign

--- components/tls/tls-client/src/sign.rs
+++ components/tls/tls-client/src/sign.rs
@@ -2,14 +2,15 @@ use crate::{
     error::Error,
     x509::{wrap_in_asn1_len, wrap_in_sequence},
 };
-use ring::{
-    io::der,
-    signature::{self, EcdsaKeyPair, Ed25519KeyPair, RsaKeyPair},
+use p256::{
+    ecdsa::{signature::Signer as _, SigningKey as EcdsaKey},
+    pkcs8::DecodePrivateKey,
 };
 use std::{convert::TryFrom, error::Error as StdError, fmt, sync::Arc};
 use tls_core::{
     key,
     msgs::enums::{SignatureAlgorithm, SignatureScheme},
+    x509::Tag,
 };
 
 /// An abstract signing key.
@@ -121,13 +122,7 @@ impl CertifiedKey {
 /// Parse `der` as any supported key encoding/type, returning
 /// the first which works.
 pub fn any_supported_type(der: &key::PrivateKey) -> Result<Arc<dyn SigningKey>, SignError> {
-    if let Ok(rsa) = RsaSigningKey::new(der) {
-        Ok(Arc::new(rsa))
-    } else if let Ok(ecdsa) = any_ecdsa_type(der) {
-        Ok(ecdsa)
-    } else {
-        any_eddsa_type(der)
-    }
+    any_ecdsa_type(der)
 }
 
 /// Parse `der` as any ECDSA key type, returning the first which works.
@@ -135,123 +130,15 @@ pub fn any_supported_type(der: &key::PrivateKey) -> Result<Arc<dyn SigningKey>, 
 /// Both SEC1 (PEM section starting with 'BEGIN EC PRIVATE KEY') and PKCS8
 /// (PEM section starting with 'BEGIN PRIVATE KEY') encodings are supported.
 pub fn any_ecdsa_type(der: &key::PrivateKey) -> Result<Arc<dyn SigningKey>, SignError> {
-    if let Ok(ecdsa_p256) = EcdsaSigningKey::new(
-        der,
-        SignatureScheme::ECDSA_NISTP256_SHA256,
-        &signature::ECDSA_P256_SHA256_ASN1_SIGNING,
-    ) {
+    if let Ok(ecdsa_p256) = EcdsaSigningKey::new(der, SignatureScheme::ECDSA_NISTP256_SHA256) {
         return Ok(Arc::new(ecdsa_p256));
     }
 
-    if let Ok(ecdsa_p384) = EcdsaSigningKey::new(
-        der,
-        SignatureScheme::ECDSA_NISTP384_SHA384,
-        &signature::ECDSA_P384_SHA384_ASN1_SIGNING,
-    ) {
+    if let Ok(ecdsa_p384) = EcdsaSigningKey::new(der, SignatureScheme::ECDSA_NISTP384_SHA384) {
         return Ok(Arc::new(ecdsa_p384));
     }
 
     Err(SignError(()))
-}
-
-/// Parse `der` as any EdDSA key type, returning the first which works.
-pub fn any_eddsa_type(der: &key::PrivateKey) -> Result<Arc<dyn SigningKey>, SignError> {
-    if let Ok(ed25519) = Ed25519SigningKey::new(der, SignatureScheme::ED25519) {
-        return Ok(Arc::new(ed25519));
-    }
-
-    // TODO: Add support for Ed448
-
-    Err(SignError(()))
-}
-
-/// A `SigningKey` for RSA-PKCS1 or RSA-PSS.
-///
-/// This is used by the test suite, so it must be `pub`, but it isn't part of
-/// the public, stable, API.
-#[doc(hidden)]
-pub struct RsaSigningKey {
-    key: Arc<RsaKeyPair>,
-}
-
-static ALL_RSA_SCHEMES: &[SignatureScheme] = &[
-    SignatureScheme::RSA_PSS_SHA512,
-    SignatureScheme::RSA_PSS_SHA384,
-    SignatureScheme::RSA_PSS_SHA256,
-    SignatureScheme::RSA_PKCS1_SHA512,
-    SignatureScheme::RSA_PKCS1_SHA384,
-    SignatureScheme::RSA_PKCS1_SHA256,
-];
-
-impl RsaSigningKey {
-    /// Make a new `RsaSigningKey` from a DER encoding, in either
-    /// PKCS#1 or PKCS#8 format.
-    pub fn new(der: &key::PrivateKey) -> Result<Self, SignError> {
-        RsaKeyPair::from_der(&der.0)
-            .or_else(|_| RsaKeyPair::from_pkcs8(&der.0))
-            .map(|s| Self { key: Arc::new(s) })
-            .map_err(|_| SignError(()))
-    }
-}
-
-impl SigningKey for RsaSigningKey {
-    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
-        ALL_RSA_SCHEMES
-            .iter()
-            .find(|scheme| offered.contains(scheme))
-            .map(|scheme| RsaSigner::new(Arc::clone(&self.key), *scheme))
-    }
-
-    fn algorithm(&self) -> SignatureAlgorithm {
-        SignatureAlgorithm::RSA
-    }
-}
-
-#[allow(clippy::upper_case_acronyms)]
-#[doc(hidden)]
-#[deprecated(since = "0.20.0", note = "Use RsaSigningKey")]
-pub type RSASigningKey = RsaSigningKey;
-
-struct RsaSigner {
-    key: Arc<RsaKeyPair>,
-    scheme: SignatureScheme,
-    encoding: &'static dyn signature::RsaEncoding,
-}
-
-impl RsaSigner {
-    fn new(key: Arc<RsaKeyPair>, scheme: SignatureScheme) -> Box<dyn Signer> {
-        let encoding: &dyn signature::RsaEncoding = match scheme {
-            SignatureScheme::RSA_PKCS1_SHA256 => &signature::RSA_PKCS1_SHA256,
-            SignatureScheme::RSA_PKCS1_SHA384 => &signature::RSA_PKCS1_SHA384,
-            SignatureScheme::RSA_PKCS1_SHA512 => &signature::RSA_PKCS1_SHA512,
-            SignatureScheme::RSA_PSS_SHA256 => &signature::RSA_PSS_SHA256,
-            SignatureScheme::RSA_PSS_SHA384 => &signature::RSA_PSS_SHA384,
-            SignatureScheme::RSA_PSS_SHA512 => &signature::RSA_PSS_SHA512,
-            _ => unreachable!(),
-        };
-
-        Box::new(Self {
-            key,
-            scheme,
-            encoding,
-        })
-    }
-}
-
-impl Signer for RsaSigner {
-    fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
-        let mut sig = vec![0; self.key.public_modulus_len()];
-
-        let rng = ring::rand::SystemRandom::new();
-        self.key
-            .sign(self.encoding, &rng, message, &mut sig)
-            .map(|_| sig)
-            .map_err(|_| Error::General("signing failed".to_string()))
-    }
-
-    fn scheme(&self) -> SignatureScheme {
-        self.scheme
-    }
 }
 
 /// A SigningKey that uses exactly one TLS-level SignatureScheme
@@ -266,7 +153,7 @@ impl Signer for RsaSigner {
 ///
 /// Currently this is only implemented for ECDSA keys.
 struct EcdsaSigningKey {
-    key: Arc<EcdsaKeyPair>,
+    key: Arc<EcdsaKey>,
     scheme: SignatureScheme,
 }
 
@@ -274,46 +161,20 @@ impl EcdsaSigningKey {
     /// Make a new `ECDSASigningKey` from a DER encoding in PKCS#8 or SEC1
     /// format, expecting a key usable with precisely the given signature
     /// scheme.
-    fn new(
-        der: &key::PrivateKey,
-        scheme: SignatureScheme,
-        sigalg: &'static signature::EcdsaSigningAlgorithm,
-    ) -> Result<Self, ()> {
-        EcdsaKeyPair::from_pkcs8(sigalg, &der.0)
+    fn new(der: &key::PrivateKey, scheme: SignatureScheme) -> Result<Self, ()> {
+        // We only support ECDSA_NISTP256_SHA256
+        match scheme {
+            SignatureScheme::ECDSA_NISTP256_SHA256 => {}
+            _ => return Err(()), // all callers are in this file
+        }
+
+        p256::SecretKey::from_pkcs8_der(&der.0)
+            .or_else(|_| p256::SecretKey::from_sec1_der(&der.0))
             .map_err(|_| ())
-            .or_else(|_| Self::convert_sec1_to_pkcs8(scheme, sigalg, &der.0))
             .map(|kp| Self {
-                key: Arc::new(kp),
+                key: Arc::new(kp.into()),
                 scheme,
             })
-    }
-
-    /// Convert a SEC1 encoding to PKCS8, and ask ring to parse it.  This
-    /// can be removed once https://github.com/briansmith/ring/pull/1456
-    /// (or equivalent) is landed.
-    fn convert_sec1_to_pkcs8(
-        scheme: SignatureScheme,
-        sigalg: &'static signature::EcdsaSigningAlgorithm,
-        maybe_sec1_der: &[u8],
-    ) -> Result<EcdsaKeyPair, ()> {
-        let pkcs8_prefix = match scheme {
-            SignatureScheme::ECDSA_NISTP256_SHA256 => &PKCS8_PREFIX_ECDSA_NISTP256,
-            SignatureScheme::ECDSA_NISTP384_SHA384 => &PKCS8_PREFIX_ECDSA_NISTP384,
-            _ => unreachable!(), // all callers are in this file
-        };
-
-        // wrap sec1 encoding in an OCTET STRING
-        let mut sec1_wrap = Vec::with_capacity(maybe_sec1_der.len() + 8);
-        sec1_wrap.extend_from_slice(maybe_sec1_der);
-        wrap_in_asn1_len(&mut sec1_wrap);
-        sec1_wrap.insert(0, der::Tag::OctetString as u8);
-
-        let mut pkcs8 = Vec::with_capacity(pkcs8_prefix.len() + sec1_wrap.len() + 4);
-        pkcs8.extend_from_slice(pkcs8_prefix);
-        pkcs8.extend_from_slice(&sec1_wrap);
-        wrap_in_sequence(&mut pkcs8);
-
-        EcdsaKeyPair::from_pkcs8(sigalg, &pkcs8).map_err(|_| ())
     }
 }
 
@@ -326,16 +187,6 @@ const PKCS8_PREFIX_ECDSA_NISTP256: &[u8] = b"\x02\x01\x00\
       \x30\x13\
       \x06\x07\x2a\x86\x48\xce\x3d\x02\x01\
       \x06\x08\x2a\x86\x48\xce\x3d\x03\x01\x07";
-
-// This is (line-by-line):
-// - INTEGER Version = 0
-// - SEQUENCE (privateKeyAlgorithm)
-//   - id-ecPublicKey OID
-//   - secp384r1 OID
-const PKCS8_PREFIX_ECDSA_NISTP384: &[u8] = b"\x02\x01\x00\
-     \x30\x10\
-     \x06\x07\x2a\x86\x48\xce\x3d\x02\x01\
-     \x06\x05\x2b\x81\x04\x00\x22";
 
 impl SigningKey for EcdsaSigningKey {
     fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
@@ -356,79 +207,16 @@ impl SigningKey for EcdsaSigningKey {
 }
 
 struct EcdsaSigner {
-    key: Arc<EcdsaKeyPair>,
+    key: Arc<EcdsaKey>,
     scheme: SignatureScheme,
 }
 
 impl Signer for EcdsaSigner {
     fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
-        let rng = ring::rand::SystemRandom::new();
-        self.key
-            .sign(&rng, message)
-            .map_err(|_| Error::General("signing failed".into()))
-            .map(|sig| sig.as_ref().into())
-    }
-
-    fn scheme(&self) -> SignatureScheme {
-        self.scheme
-    }
-}
-
-/// A SigningKey that uses exactly one TLS-level SignatureScheme
-/// and one ring-level signature::SigningAlgorithm.
-///
-/// Compare this to RsaSigningKey, which for a particular key is
-/// willing to sign with several algorithms.  This is quite poor
-/// cryptography practice, but is necessary because a given RSA key
-/// is expected to work in TLS1.2 (PKCS#1 signatures) and TLS1.3
-/// (PSS signatures) -- nobody is willing to obtain certificates for
-/// different protocol versions.
-///
-/// Currently this is only implemented for Ed25519 keys.
-struct Ed25519SigningKey {
-    key: Arc<Ed25519KeyPair>,
-    scheme: SignatureScheme,
-}
-
-impl Ed25519SigningKey {
-    /// Make a new `Ed25519SigningKey` from a DER encoding in PKCS#8 format,
-    /// expecting a key usable with precisely the given signature scheme.
-    fn new(der: &key::PrivateKey, scheme: SignatureScheme) -> Result<Self, SignError> {
-        Ed25519KeyPair::from_pkcs8_maybe_unchecked(&der.0)
-            .map(|kp| Self {
-                key: Arc::new(kp),
-                scheme,
-            })
-            .map_err(|_| SignError(()))
-    }
-}
-
-impl SigningKey for Ed25519SigningKey {
-    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
-        if offered.contains(&self.scheme) {
-            Some(Box::new(Ed25519Signer {
-                key: Arc::clone(&self.key),
-                scheme: self.scheme,
-            }))
-        } else {
-            None
-        }
-    }
-
-    fn algorithm(&self) -> SignatureAlgorithm {
-        use tls_core::msgs::handshake::DecomposedSignatureScheme;
-        self.scheme.sign()
-    }
-}
-
-struct Ed25519Signer {
-    key: Arc<Ed25519KeyPair>,
-    scheme: SignatureScheme,
-}
-
-impl Signer for Ed25519Signer {
-    fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
-        Ok(self.key.sign(message).as_ref().into())
+        Ok(
+            p256::ecdsa::signature::Signer::<p256::ecdsa::Signature>::sign(&(*self.key), message)
+                .to_vec(),
+        )
     }
 
     fn scheme(&self) -> SignatureScheme {
@@ -439,14 +227,7 @@ impl Signer for Ed25519Signer {
 /// The set of schemes we support for signatures and
 /// that are allowed for TLS1.3.
 pub fn supported_sign_tls13() -> &'static [SignatureScheme] {
-    &[
-        SignatureScheme::ECDSA_NISTP384_SHA384,
-        SignatureScheme::ECDSA_NISTP256_SHA256,
-        SignatureScheme::RSA_PSS_SHA512,
-        SignatureScheme::RSA_PSS_SHA384,
-        SignatureScheme::RSA_PSS_SHA256,
-        SignatureScheme::ED25519,
-    ]
+    &[SignatureScheme::ECDSA_NISTP256_SHA256]
 }
 
 /// Errors while signing
@@ -466,7 +247,6 @@ fn can_load_ecdsa_nistp256_pkcs8() {
     let key = key::PrivateKey(include_bytes!("testdata/nistp256key.pkcs8.der").to_vec());
     assert!(any_supported_type(&key).is_ok());
     assert!(any_ecdsa_type(&key).is_ok());
-    assert!(any_eddsa_type(&key).is_err());
 }
 
 #[test]
@@ -474,45 +254,4 @@ fn can_load_ecdsa_nistp256_sec1() {
     let key = key::PrivateKey(include_bytes!("testdata/nistp256key.der").to_vec());
     assert!(any_supported_type(&key).is_ok());
     assert!(any_ecdsa_type(&key).is_ok());
-    assert!(any_eddsa_type(&key).is_err());
-}
-
-#[test]
-fn can_load_ecdsa_nistp384_pkcs8() {
-    let key = key::PrivateKey(include_bytes!("testdata/nistp384key.pkcs8.der").to_vec());
-    assert!(any_supported_type(&key).is_ok());
-    assert!(any_ecdsa_type(&key).is_ok());
-    assert!(any_eddsa_type(&key).is_err());
-}
-
-#[test]
-fn can_load_ecdsa_nistp384_sec1() {
-    let key = key::PrivateKey(include_bytes!("testdata/nistp384key.der").to_vec());
-    assert!(any_supported_type(&key).is_ok());
-    assert!(any_ecdsa_type(&key).is_ok());
-    assert!(any_eddsa_type(&key).is_err());
-}
-
-#[test]
-fn can_load_eddsa_pkcs8() {
-    let key = key::PrivateKey(include_bytes!("testdata/eddsakey.der").to_vec());
-    assert!(any_supported_type(&key).is_ok());
-    assert!(any_eddsa_type(&key).is_ok());
-    assert!(any_ecdsa_type(&key).is_err());
-}
-
-#[test]
-fn can_load_rsa2048_pkcs8() {
-    let key = key::PrivateKey(include_bytes!("testdata/rsa2048key.pkcs8.der").to_vec());
-    assert!(any_supported_type(&key).is_ok());
-    assert!(any_eddsa_type(&key).is_err());
-    assert!(any_ecdsa_type(&key).is_err());
-}
-
-#[test]
-fn can_load_rsa2048_pkcs1() {
-    let key = key::PrivateKey(include_bytes!("testdata/rsa2048key.pkcs1.der").to_vec());
-    assert!(any_supported_type(&key).is_ok());
-    assert!(any_eddsa_type(&key).is_err());
-    assert!(any_ecdsa_type(&key).is_err());
 }
