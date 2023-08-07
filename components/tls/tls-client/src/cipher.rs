ce74024453d167c2095e925c7841f48bdbfaf262
sinu <65924192+sinui0@users.noreply.github.com>
2023-08-07 14:36:52 -0700

    remove from cipher mod

--- components/tls/tls-client/src/cipher.rs
+++ components/tls/tls-client/src/cipher.rs
@@ -1,6 +1,5 @@
 use crate::Error;
 use async_trait::async_trait;
-use ring::{aead, hkdf};
 use tls_core::msgs::{
     codec,
     message::{OpaqueMessage, PlainMessage},
@@ -38,55 +37,4 @@ impl MessageDecrypter for InvalidMessageDecrypter {
     async fn decrypt(&self, _m: OpaqueMessage, _seq: u64) -> Result<PlainMessage, Error> {
         Err(Error::DecryptError)
     }
-}
-
-/// A write or read IV.
-#[derive(Default)]
-pub(crate) struct Iv(pub(crate) [u8; ring::aead::NONCE_LEN]);
-
-impl Iv {
-    #[cfg(feature = "tls12")]
-    fn new(value: [u8; ring::aead::NONCE_LEN]) -> Self {
-        Self(value)
-    }
-
-    #[cfg(feature = "tls12")]
-    pub(crate) fn copy(value: &[u8]) -> Self {
-        debug_assert_eq!(value.len(), ring::aead::NONCE_LEN);
-        let mut iv = Self::new(Default::default());
-        iv.0.copy_from_slice(value);
-        iv
-    }
-
-    #[cfg(test)]
-    pub(crate) fn value(&self) -> &[u8; 12] {
-        &self.0
-    }
-}
-
-pub(crate) struct IvLen;
-
-impl hkdf::KeyType for IvLen {
-    fn len(&self) -> usize {
-        aead::NONCE_LEN
-    }
-}
-
-impl From<hkdf::Okm<'_, IvLen>> for Iv {
-    fn from(okm: hkdf::Okm<IvLen>) -> Self {
-        let mut r = Self(Default::default());
-        okm.fill(&mut r.0[..]).unwrap();
-        r
-    }
-}
-
-pub(crate) fn make_nonce(iv: &Iv, seq: u64) -> ring::aead::Nonce {
-    let mut nonce = [0u8; ring::aead::NONCE_LEN];
-    codec::put_u64(seq, &mut nonce[4..]);
-
-    nonce.iter_mut().zip(iv.0.iter()).for_each(|(nonce, iv)| {
-        *nonce ^= *iv;
-    });
-
-    aead::Nonce::assume_unique_for_key(nonce)
 }
