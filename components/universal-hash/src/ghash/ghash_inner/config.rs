097337c3c9155b52da9393a22673a52bb3a28adb
sinu.eth <65924192+sinui0@users.noreply.github.com>
2023-08-31 11:14:28 -0700

    fix: bump GHASH default max block length to 1026 (#313)

--- components/universal-hash/src/ghash/ghash_inner/config.rs
+++ components/universal-hash/src/ghash/ghash_inner/config.rs
@@ -7,10 +7,10 @@ pub struct GhashConfig {
     #[builder(setter(into))]
     pub id: String,
     /// Initial number of block shares to provision
-    #[builder(default = "1024")]
+    #[builder(default = "1026")]
     pub initial_block_count: usize,
     /// Maximum number of blocks supported
-    #[builder(default = "1024")]
+    #[builder(default = "1026")]
     pub max_block_count: usize,
 }
 
