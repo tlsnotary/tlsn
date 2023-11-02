bffa3bb4d97dbf9215bbe64a272d822eec2f7f6b
th4s <th4s@metavoid.xyz>
2023-11-02 15:47:46 +0100

    Repair notarize integration test

--- tlsn/tests-integration/tests/notarize.rs
+++ tlsn/tests-integration/tests/notarize.rs
@@ -73,9 +73,17 @@ async fn prover<T: AsyncWrite + AsyncRead + Send + Unpin + 'static>(notary_socke
 
     client_socket.close().await.unwrap();
 
-    let prover = prover_task.await.unwrap().unwrap().start_notarize();
+    let mut prover = prover_task.await.unwrap().unwrap().start_notarize();
+    let sent_tx_len = prover.sent_transcript().data().len();
+    let recv_tx_len = prover.recv_transcript().data().len();
 
-    _ = prover.finalize().await.unwrap();
+    let builder = prover.commitment_builder();
+
+    // Commit to everything
+    builder.commit_sent(0..sent_tx_len).unwrap();
+    builder.commit_recv(0..recv_tx_len).unwrap();
+
+    let _notarized_session = prover.finalize().await.unwrap();
 }
 
 #[instrument(skip(socket))]
