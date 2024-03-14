5bea279169129bef0dc2aacbf35c686d519b43d4
Nick Tampakas <nick@gmx.co.uk>
2024-03-14 09:22:06 +0200

    Modify sercice validation script

--- appspec-scripts/validate_app.sh
+++ appspec-scripts/validate_app.sh
@@ -4,17 +4,17 @@ set -e
 # Verify proccess is running
 APP_NAME=$(echo $APPLICATION_NAME | awk -F- '{ print $2 }')
 
-pgrep -f notary.*$APP_NAME
-[ $? -eq 0 ] || exit 1
+#pgrep -f notary.*$APP_NAME
+#[ $? -eq 0 ] || exit 1
 
 # Verify that listening sockets exist
-if [ "$APPLICATION_NAME" == "tlsn-nightly" ]; then
-    port=7048
+if [ $APP_NAME = "stable" ]; then
+  PORT=$(curl http://169.254.169.254/latest/meta-data/tags/instance/port)
 else
-    port=7047
+  PORT=7048
 fi
 
-exposed_ports=$(netstat -lnt4 | egrep -cw $port)
-[ $exposed_ports -eq 1 ] || exit 1
+EXPOSED_PORTS=$(netstat -lnt4 | egrep -cw $PORT)
+[ $EXPOSED_PORTS -eq 1 ] || exit 1
 
 exit 0
