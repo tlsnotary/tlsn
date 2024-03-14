9796aef78ff0a856cfe3688995d3b80a5e2874c1
Nick Tampakas <nick@gmx.co.uk>
2024-03-14 09:21:20 +0200

    Modify script to start service for the 3 latest stable versions

--- appspec-scripts/start_app.sh
+++ appspec-scripts/start_app.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-set -e
+set -x
 
+TAG=$(curl http://169.254.169.254/latest/meta-data/tags/instance/stable)
 APP_NAME=$(echo $APPLICATION_NAME | awk -F- '{ print $2 }')
 
-cd ~/$APP_NAME/tlsn/notary-server
-target/release/notary-server --config-file ~/.notary/$APP_NAME/config.yaml &> ~/$APP_NAME/tlsn/notary.log &
+if [ $APP_NAME = "stable" ]; then
+  STABLE_PORTS="7047 7057 7067"
+  for PORT in $STABLE_PORTS; do
+    PORT_LISTENING=$(netstat -lnt4 | egrep -cw $PORT)
+    if [ $PORT_LISTENING -eq 0 ]; then
+      cd ~/${APP_NAME}_${TAG}/tlsn/notary-server
+      target/release/notary-server --config-file ~/.notary/${APP_NAME}_${PORT}/config.yaml &> ~/${APP_NAME}_${TAG}/tlsn/notary.log &
+      # Create a tag that will be used for service validation
+      INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+      aws ec2 create-tags --resources $INSTANCE_ID --tags "Key=port,Value=$PORT"
+    break
+    fi
+  done
+else
+  cd ~/$APP_NAME/tlsn/notary-server
+  target/release/notary-server --config-file ~/.notary/$APP_NAME/config.yaml &> ~/$APP_NAME/tlsn/notary.log &
+fi
 
 exit 0
