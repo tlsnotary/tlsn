545492dbced526d74c3d7778c1c6379293ab6fb2
Nick Tampakas <nick@gmx.co.uk>
2024-03-14 09:04:53 +0200

    Modify script to run only in nightly env

--- appspec-scripts/before_install.sh
+++ appspec-scripts/before_install.sh
@@ -3,8 +3,10 @@
 
 APP_NAME=$(echo $APPLICATION_NAME | awk -F- '{ print $2 }')
 
-if [ ! -d $APP_NAME ]; then
-   mkdir ~/$APP_NAME
+if [ $APP_NAME = "nightly" ]; then
+  if [ ! -d $APP_NAME ]; then
+    mkdir ~/$APP_NAME
+  fi
 fi
 
 exit 0
