7582248a24dd34498bc55e947694cbedceaf2cd5
Nick Tampakas <nick@gmx.co.uk>
2024-03-14 11:04:37 +0200

    Enhancement of validation script

--- appspec-scripts/validate_app.sh
+++ appspec-scripts/validate_app.sh
@@ -4,14 +4,15 @@ set -e
 # Verify proccess is running
 APP_NAME=$(echo $APPLICATION_NAME | awk -F- '{ print $2 }')
 
-#pgrep -f notary.*$APP_NAME
-#[ $? -eq 0 ] || exit 1
-
 # Verify that listening sockets exist
 if [ $APP_NAME = "stable" ]; then
   PORT=$(curl http://169.254.169.254/latest/meta-data/tags/instance/port)
+  ps -ef | grep notary.*$APP_NAME.*$PORT | grep -v grep
+  [ $? -eq 0 ] || exit 1
 else
   PORT=7048
+  pgrep -f notary.*$APP_NAME
+  [ $? -eq 0 ] || exit 1
 fi
 
 EXPOSED_PORTS=$(netstat -lnt4 | egrep -cw $PORT)
