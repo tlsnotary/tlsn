c7e62b53955b5ce646d15d56879dd0a8fc5ba264
Nick Tampakas <nick@gmx.co.uk>
2024-03-26 11:00:58 +0200

    Check if all stable ports are in use and terminate

--- cd-scripts/appspec-scripts/start_app.sh
+++ cd-scripts/appspec-scripts/start_app.sh
@@ -6,6 +6,8 @@ TAG=$(curl http://169.254.169.254/latest/meta-data/tags/instance/stable)
 APP_NAME=$(echo $APPLICATION_NAME | awk -F- '{ print $2 }')
 
 if [ $APP_NAME = "stable" ]; then
+  # Check if all stable ports are in use. If true, terminate the deployment
+  [[ $(netstat -lnt4 | egrep -c ':(7047|7057|7067)\s') -eq 3 ]] && { echo "All stable ports are in use"; exit 1; }
   STABLE_PORTS="7047 7057 7067"
   for PORT in $STABLE_PORTS; do
     PORT_LISTENING=$(netstat -lnt4 | egrep -cw $PORT || true)
