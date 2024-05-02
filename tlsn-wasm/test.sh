e63de31b51e27216e25e0b5f184f0653328a14ac
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-05-02 09:26:03 +0200

    added test script

--- tlsn-wasm/test.sh
+++ tlsn-wasm/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals" \
     rustup run nightly \
-    wasm-pack build --target web \
+    wasm-pack test --headless --chrome \
     . -Z build-std=panic_abort,std
