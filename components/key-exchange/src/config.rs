f2578538ab41ab1bf48e82cc89f4e1852b061271
sinu.eth <65924192+sinui0@users.noreply.github.com>
2024-06-13 11:20:09 -0700

    refactor(key-exchange): mpz upgrade (#496)
    * refactor: mpz upgrade key exchange
    
    * Update components/key-exchange/src/config.rs
    
    Co-authored-by: th4s <th4s@metavoid.xyz>
    
    * Apply suggestions from code review
    
    Co-authored-by: dan <themighty1@users.noreply.github.com>
    
    * use chacha12 for tests
    
    ---------
    
    Co-authored-by: th4s <th4s@metavoid.xyz>
    Co-authored-by: dan <themighty1@users.noreply.github.com>

--- components/key-exchange/src/config.rs
+++ components/key-exchange/src/config.rs
@@ -1,21 +1,17 @@
-//! This module provides the [KeyExchangeConfig] for configuration of the key exchange instance.
-
 use derive_builder::Builder;
 
 /// Role in the key exchange protocol.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[allow(missing_docs)]
 pub enum Role {
+    /// Leader.
     Leader,
+    /// Follower.
     Follower,
 }
 
-/// A config used for [KeyExchangeCore](super::KeyExchangeCore).
+/// A config used for [MpcKeyExchange](super::MpcKeyExchange).
 #[derive(Debug, Clone, Builder)]
 pub struct KeyExchangeConfig {
-    /// The id of this instance.
-    #[builder(setter(into))]
-    id: String,
     /// Protocol role.
     role: Role,
 }
@@ -24,11 +20,6 @@ impl KeyExchangeConfig {
     /// Creates a new builder for the key exchange configuration.
     pub fn builder() -> KeyExchangeConfigBuilder {
         KeyExchangeConfigBuilder::default()
-    }
-
-    /// Get the id of this instance.
-    pub fn id(&self) -> &str {
-        &self.id
     }
 
     /// Get the role of this instance.
