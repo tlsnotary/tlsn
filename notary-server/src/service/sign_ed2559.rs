aa4d62ba955e0498691e338e42f42d925bb33c9f
asa93 <asa93@live.fr>
2024-07-01 22:25:05 -0400

    notary/signer: remov duplicate pkey field

--- notary-server/src/service/sign_ed2559.rs
+++ notary-server/src/service/sign_ed2559.rs
@@ -2,7 +2,6 @@ use ed25519_dalek::SigningKey;
 use ed25519_dalek::{Signature, Signer, Verifier};
 /// Signer256k1 to generate Scp256k1 signature
 pub(crate) struct SignerEd25519 {
-    private_key: [u8; 32],
     pub signing_key: SigningKey,
 }
 
@@ -12,10 +11,7 @@ impl SignerEd25519 {
         let private_key: [u8; 32] = hex::decode(private_key).unwrap().try_into().unwrap();
         let signing_key: SigningKey = SigningKey::from_bytes(&private_key);
 
-        SignerEd25519 {
-            private_key,
-            signing_key,
-        }
+        SignerEd25519 { signing_key }
     }
 
     pub(crate) fn sign(&self, message: impl AsRef<[u8]>) -> Signature {
@@ -37,7 +33,7 @@ mod test {
         let private_key_env = std::env::var("NOTARY_PRIVATE_KEY_SECP256k1").unwrap();
         println!("private_key {:}", private_key_env);
         let signer = SignerEd25519::new(private_key_env);
-        println!("signing_key {:#?}", signer.signing_key);
+        println!("signing_key {:?}", signer.signing_key.as_bytes());
 
         let message: String = String::from("This is a test of the tsunami alert system.");
         let signature: Signature = signer.sign(message.clone());
