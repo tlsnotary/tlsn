e14d0cf563e866cde18d8cf7a79cfbe66d220acd
sinu.eth <65924192+sinui0@users.noreply.github.com>
2024-07-23 10:43:31 +0900

    refactor: consolidate workspaces (#537)
    * move crates
    
    * remove .workspace syntax
    
    * rename crate directories
    
    * normalize manifests
    
    * further normalize manifests
    
    * newlines
    
    * fix dependency issues
    
    * fix import paths
    
    * update CI
    
    * rustfmt
    
    * filter wasm packages
    
    * check in fixtures
    
    * fix rustdoc
    
    * ignore expensive tests

---
+++ crates/examples/src/lib.rs
@@ -0,0 +1,22 @@
+use elliptic_curve::pkcs8::DecodePrivateKey;
+use futures::{AsyncRead, AsyncWrite};
+use tlsn_verifier::tls::{Verifier, VerifierConfig};
+
+/// Runs a simple Notary with the provided connection to the Prover.
+pub async fn run_notary<T: AsyncWrite + AsyncRead + Send + Unpin + 'static>(conn: T) {
+    // Load the notary signing key
+    let signing_key_str = std::str::from_utf8(include_bytes!(
+        "../../notary/server/fixture/notary/notary.key"
+    ))
+    .unwrap();
+    let signing_key = p256::ecdsa::SigningKey::from_pkcs8_pem(signing_key_str).unwrap();
+
+    // Setup default config. Normally a different ID would be generated
+    // for each notarization.
+    let config = VerifierConfig::builder().id("example").build().unwrap();
+
+    Verifier::new(config)
+        .notarize::<_, p256::ecdsa::Signature>(conn, &signing_key)
+        .await
+        .unwrap();
+}
