603125496346dc5aae44a9786dad62edd0f33d0e
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-08-01 14:40:59 +0200

    chore: cleanup after workspace consolidation
    * clean up github actions #537 #536
    * Improved README.md (clang + openssl-dev) #536 
    + removed stale build script #537 
    * chore: add build script for tlsn-wasm #536
    
    #549

--- crates/wasm/build.sh
+++ crates/wasm/build.sh
@@ -1,13 +1,3 @@
-# TLSNotary WASM bindings
-
-## Build
-
-This crate must be built using the nightly rust compiler with the following flags:
-
-```bash
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
     rustup run nightly \
     wasm-pack build --target web . -- -Zbuild-std=panic_abort,std
-```
-
-
