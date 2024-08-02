497bf85f55f9656ab117dcb368f995541f5002c8
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-08-02 12:28:08 +0200

    fix: patch tlsn_wasm import in rayon worker helper

--- crates/wasm/build.sh
+++ crates/wasm/build.sh
@@ -1,3 +1,20 @@
+#!/bin/sh
+set -e
+
+# Clean up older builds
+rm -rf pkg
+
+# Build tlsn_wasm package
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
     rustup run nightly \
     wasm-pack build --target web . -- -Zbuild-std=panic_abort,std
+
+# Patch tlsn_wasm.js import in workerHelpers.worker.js
+file=$(ls ./pkg/snippets/wasm-bindgen-rayon-*/src/workerHelpers.worker.js)
+temp=$(mktemp)
+sed 's|../../../|../../../tlsn_wasm.js|' "$file" >"$temp" && mv "$temp" "$file"
+
+# Add snippets directory to package.json
+file="pkg/package.json"
+temp=$(mktemp)
+jq '.files += ["snippets/"]' "$file" >"$temp" && mv "$temp" "$file"
