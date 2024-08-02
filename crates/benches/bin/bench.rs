b325c2f33bb221f500bc9968fe97c00af542e2d2
yuroitaki <>
2024-08-02 14:50:16 +0800

    Apply fmt.

--- crates/benches/bin/bench.rs
+++ crates/benches/bin/bench.rs
@@ -5,8 +5,8 @@ use tlsn_benches::{clean_up, set_up};
 fn main() {
     let prover_path =
         std::env::var("PROVER_PATH").unwrap_or_else(|_| "../../target/release/prover".to_string());
-    let verifier_path =
-        std::env::var("VERIFIER_PATH").unwrap_or_else(|_| "../../target/release/verifier".to_string());
+    let verifier_path = std::env::var("VERIFIER_PATH")
+        .unwrap_or_else(|_| "../../target/release/verifier".to_string());
 
     if let Err(e) = set_up() {
         println!("Error setting up: {}", e);
