cf331af03f36b4afc379e0769434236d62128188
AmirSaber Sharifi <inbox@saberistic.com>
2024-08-26 15:51:21 -0500

    use zeroize and detecting paths

--- crates/verifier/src/tls/notarize.rs
+++ crates/verifier/src/tls/notarize.rs
@@ -10,6 +10,7 @@ use tlsn_core::{msg::SignedSession, Signature};
 
 use sha2::{Digest, Sha256};
 use tracing::{debug, info, instrument};
+use zeroize::Zeroize;
 
 impl Verifier<Notarize> {
     /// Notarizes the TLS session.
@@ -27,24 +28,49 @@ impl Verifier<Notarize> {
             mut io,
             mux_ctrl,
             mut mux_fut,
-            response_data,
-            request_data,
+            mut response_data,
+            mut request_data,
             ..
         } = self.state;
 
         let mut request_headers = [httparse::EMPTY_HEADER; 16];
         let mut request = Request::new(&mut request_headers);
-        let _req_result = request.parse(request_data.as_bytes()).unwrap();
+        let request_data_mut = request_data.to_owned();
+        let req_bytes = request_data_mut.as_bytes();
+        let _req_result = request.parse(&req_bytes).unwrap();
 
         let mut response_headers = [httparse::EMPTY_HEADER; 16];
         let mut response = Response::new(&mut response_headers);
-        let _resp_result = response.parse(response_data.as_bytes()).unwrap();
+        let response_data_mut = response_data.to_owned();
+        let resp_bytes = response_data_mut.as_bytes();
+        let _resp_result = response.parse(resp_bytes).unwrap();
+        
+        
+        match request.path {
+            Some(path) => {
+                info!("request path: {:?}", path);
+                match path {
+                    "https://swapi.dev/api/people/1" => {
+                        info!("swapi path: {:?}", path);
+                    }
+                    "https://api.x.com/1.1/account/settings.json" => {
+                        info!("x.com settings path: {:?}", path);
+                    }
+                    _ => {
+                        info!("request path not found");
+                    }
+                }
+            }
+            None => {
+                info!("request path not found");
+            }
+        }
 
         let session_header = mux_fut
             .poll_with(async {
                 let mut data = Vec::new();
-                data.extend_from_slice(response_data.as_bytes());
-                data.extend_from_slice(request_data.as_bytes());
+                data.extend_from_slice(req_bytes);
+                data.extend_from_slice(resp_bytes);
                 let mut hasher = Sha256::new();
                 hasher.update(&data);
                 let hash = hasher.finalize();
@@ -66,6 +92,11 @@ impl Verifier<Notarize> {
                 Ok::<_, VerifierError>(signed_session)
             })
             .await?;
+
+        request_data.zeroize();
+        response_data.zeroize();
+        drop(response);
+        drop(request);
 
         if !mux_fut.is_complete() {
             mux_ctrl.mux().close();
