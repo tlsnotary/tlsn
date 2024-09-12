5c7716a64669ab9414e9f060a670837161052e28
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-09-12 11:05:29 +0200

    Restored Discord example

---
+++ crates/examples/discord/discord_dm.rs
@@ -0,0 +1,233 @@
+// This example shows how to notarize Discord DMs.
+//
+// The example uses the notary server implemented in ../../notary/server
+
+use http_body_util::{BodyExt, Empty};
+use hyper::{body::Bytes, Request, StatusCode};
+use hyper_util::rt::TokioIo;
+use notary_client::{Accepted, NotarizationRequest, NotaryClient};
+use std::{env, ops::Range, str};
+use tlsn_common::config::ProtocolConfig;
+use tlsn_core::proof::TlsProof;
+use tlsn_prover::tls::{Prover, ProverConfig};
+use tokio::io::AsyncWriteExt as _;
+use tokio_util::compat::{FuturesAsyncReadCompatExt, TokioAsyncReadCompatExt};
+use tracing::debug;
+
+// Setting of the application server
+const SERVER_DOMAIN: &str = "discord.com";
+
+// Setting of the notary server — make sure these are the same with the config in ../../notary/server
+const NOTARY_HOST: &str = "127.0.0.1";
+const NOTARY_PORT: u16 = 7047;
+
+// Maximum number of bytes that can be sent from prover to server
+const MAX_SENT_DATA: usize = 1 << 12;
+// Maximum number of bytes that can be received by prover from server
+const MAX_RECV_DATA: usize = 1 << 14;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    // Load secret variables frome environment for discord server connection
+    dotenv::dotenv().ok();
+    let channel_id = env::var("CHANNEL_ID").unwrap();
+    let auth_token = env::var("AUTHORIZATION").unwrap();
+    let user_agent = env::var("USER_AGENT").unwrap();
+
+    // Build a client to connect to the notary server.
+    let notary_client = NotaryClient::builder()
+        .host(NOTARY_HOST)
+        .port(NOTARY_PORT)
+        // WARNING: Always use TLS to connect to notary server, except if notary is running locally
+        // e.g. this example, hence `enable_tls` is set to False (else it always defaults to True).
+        .enable_tls(false)
+        .build()
+        .unwrap();
+
+    // Send requests for configuration and notarization to the notary server.
+    let notarization_request = NotarizationRequest::builder()
+        .max_sent_data(MAX_SENT_DATA)
+        .max_recv_data(MAX_RECV_DATA)
+        .build()
+        .unwrap();
+
+    let Accepted {
+        io: notary_connection,
+        id: session_id,
+        ..
+    } = notary_client
+        .request_notarization(notarization_request)
+        .await
+        .unwrap();
+
+    // Set up protocol configuration for prover.
+    let protocol_config = ProtocolConfig::builder()
+        .max_sent_data(MAX_SENT_DATA)
+        .max_recv_data(MAX_RECV_DATA)
+        .build()
+        .unwrap();
+
+    // Configure a new prover with the unique session id returned from notary client.
+    let prover_config = ProverConfig::builder()
+        .id(session_id)
+        .server_dns(SERVER_DOMAIN)
+        .protocol_config(protocol_config)
+        .build()
+        .unwrap();
+
+    // Create a new prover and set up the MPC backend.
+    let prover = Prover::new(prover_config)
+        .setup(notary_connection.compat())
+        .await
+        .unwrap();
+
+    // Open a new socket to the application server.
+    let client_socket = tokio::net::TcpStream::connect((SERVER_DOMAIN, 443))
+        .await
+        .unwrap();
+
+    // Bind the Prover to server connection
+    let (tls_connection, prover_fut) = prover.connect(client_socket.compat()).await.unwrap();
+
+    // Spawn the Prover to be run concurrently
+    let prover_task = tokio::spawn(prover_fut);
+
+    // Attach the hyper HTTP client to the TLS connection
+    let (mut request_sender, connection) =
+        hyper::client::conn::http1::handshake(TokioIo::new(tls_connection.compat()))
+            .await
+            .unwrap();
+
+    // Spawn the HTTP task to be run concurrently
+    tokio::spawn(connection);
+
+    // Build the HTTP request to fetch the DMs
+    let request = Request::builder()
+        .uri(format!(
+            "https://{SERVER_DOMAIN}/api/v9/channels/{channel_id}/messages?limit=2"
+        ))
+        .header("Host", SERVER_DOMAIN)
+        .header("Accept", "*/*")
+        .header("Accept-Language", "en-US,en;q=0.5")
+        .header("Accept-Encoding", "identity")
+        .header("User-Agent", user_agent)
+        .header("Authorization", &auth_token)
+        .header("Connection", "close")
+        .body(Empty::<Bytes>::new())
+        .unwrap();
+
+    debug!("Sending request");
+
+    let response = request_sender.send_request(request).await.unwrap();
+
+    debug!("Sent request");
+
+    assert!(response.status() == StatusCode::OK, "{}", response.status());
+
+    debug!("Request OK");
+
+    // Pretty printing :)
+    let payload = response.into_body().collect().await.unwrap().to_bytes();
+    let parsed =
+        serde_json::from_str::<serde_json::Value>(&String::from_utf8_lossy(&payload)).unwrap();
+    debug!("{}", serde_json::to_string_pretty(&parsed).unwrap());
+
+    // The Prover task should be done now, so we can grab it.
+    let prover = prover_task.await.unwrap().unwrap();
+
+    // Prepare for notarization
+    let mut prover = prover.start_notarize();
+
+    // Identify the ranges in the transcript that contain secrets
+    let (public_ranges, private_ranges) =
+        find_ranges(prover.sent_transcript().data(), &[auth_token.as_bytes()]);
+
+    let recv_len = prover.recv_transcript().data().len();
+
+    let builder = prover.commitment_builder();
+
+    // Collect commitment ids for the outbound transcript
+    let mut commitment_ids = public_ranges
+        .iter()
+        .chain(private_ranges.iter())
+        .map(|range| builder.commit_sent(range).unwrap())
+        .collect::<Vec<_>>();
+
+    // Commit to the full received transcript in one shot, as we don't need to redact anything
+    commitment_ids.push(builder.commit_recv(&(0..recv_len)).unwrap());
+
+    // Finalize, returning the notarized session
+    let notarized_session = prover.finalize().await.unwrap();
+
+    debug!("Notarization complete!");
+
+    // Dump the notarized session to a file
+    let mut file = tokio::fs::File::create("discord_dm_notarized_session.json")
+        .await
+        .unwrap();
+    file.write_all(
+        serde_json::to_string_pretty(&notarized_session)
+            .unwrap()
+            .as_bytes(),
+    )
+    .await
+    .unwrap();
+
+    let session_proof = notarized_session.session_proof();
+
+    let mut proof_builder = notarized_session.data().build_substrings_proof();
+
+    // Reveal everything but the auth token (which was assigned commitment id 2)
+    proof_builder.reveal_by_id(commitment_ids[0]).unwrap();
+    proof_builder.reveal_by_id(commitment_ids[1]).unwrap();
+    proof_builder.reveal_by_id(commitment_ids[3]).unwrap();
+
+    let substrings_proof = proof_builder.build().unwrap();
+
+    let proof = TlsProof {
+        session: session_proof,
+        substrings: substrings_proof,
+    };
+
+    // Dump the proof to a file.
+    let mut file = tokio::fs::File::create("discord_dm_proof.json")
+        .await
+        .unwrap();
+    file.write_all(serde_json::to_string_pretty(&proof).unwrap().as_bytes())
+        .await
+        .unwrap();
+}
+
+/// Find the ranges of the public and private parts of a sequence.
+///
+/// Returns a tuple of `(public, private)` ranges.
+fn find_ranges(seq: &[u8], sub_seq: &[&[u8]]) -> (Vec<Range<usize>>, Vec<Range<usize>>) {
+    let mut private_ranges = Vec::new();
+    for s in sub_seq {
+        for (idx, w) in seq.windows(s.len()).enumerate() {
+            if w == *s {
+                private_ranges.push(idx..(idx + w.len()));
+            }
+        }
+    }
+
+    let mut sorted_ranges = private_ranges.clone();
+    sorted_ranges.sort_by_key(|r| r.start);
+
+    let mut public_ranges = Vec::new();
+    let mut last_end = 0;
+    for r in sorted_ranges {
+        if r.start > last_end {
+            public_ranges.push(last_end..r.start);
+        }
+        last_end = r.end;
+    }
+
+    if last_end < seq.len() {
+        public_ranges.push(last_end..seq.len());
+    }
+
+    (public_ranges, private_ranges)
+}
