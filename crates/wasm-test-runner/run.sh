1dd51cc5332a1ab6f349391c0c4fbd1d1d93ee0f
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-10-02 11:40:50 +0200

    Add convenience script to run most ci checks locally

--- crates/wasm-test-runner/run.sh
+++ crates/wasm-test-runner/run.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
+
+# Ensure the script runs in the folder that contains this script
+cd "$(dirname "$0")"
+
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
-rustup run nightly \
-wasm-pack build ../wasm --target web --no-pack --out-dir=../wasm-test-runner/static/generated -- -Zbuild-std=panic_abort,std --features test,no-bundler \
-&& RUST_LOG=debug cargo run --release
+    rustup run nightly \
+    wasm-pack build ../wasm --target web --no-pack --out-dir=../wasm-test-runner/static/generated -- -Zbuild-std=panic_abort,std --features test,no-bundler &&
+    RUST_LOG=debug cargo run --release
