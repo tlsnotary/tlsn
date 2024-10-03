ab825b4afb6e81050a096acdd2900fa58285fcfc
Valentin Mihov <valentin.mihov@gmail.com>
2024-10-03 09:21:55 +0300

    Preprocess the PRF circuits
    This preprocessing is making large heap allocations, which are done
    once and we want to remove them from the memory profiling. The
    preprocessing is done before the benchies are done, so that we
    exclude these allocations from the memory profiling.

--- crates/benches/bin/prover.rs
+++ crates/benches/bin/prover.rs
@@ -9,20 +9,14 @@ use std::{
 
 use anyhow::Context;
 use futures::{AsyncReadExt, AsyncWriteExt};
-use hmac_sha256::{MpcPrf, PrfConfig, Role, Prf};
 use tlsn_benches::{
-    config::{BenchInstance, Config},
-    metrics::Metrics,
-    set_interface, PROVER_INTERFACE,
+    config::{BenchInstance, Config}, metrics::Metrics, preprocess_prf_circuits, set_interface, PROVER_INTERFACE
 };
 
 use tlsn_common::config::ProtocolConfig;
 use tlsn_core::Direction;
 use tlsn_server_fixture::bind;
 use tlsn_server_fixture_certs::{CA_CERT_DER, SERVER_DOMAIN};
-use mpz_common::executor::test_st_executor;
-use mpz_garble::{config::Role as DEAPRole, protocol::deap::DEAPThread, Memory};
-use mpz_ot::ideal::ot::ideal_ot;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_util::{
     compat::TokioAsyncReadCompatExt,
@@ -86,73 +80,6 @@ async fn main() -> anyhow::Result<()> {
     file.flush()?;
 
     Ok(())
-}
-
-async fn preprocess_prf_circuits() {
-    let pms = [42u8; 32];
-    let client_random = [69u8; 32];
-
-    let (leader_ctx_0, follower_ctx_0) = test_st_executor(128);
-    let (leader_ctx_1, follower_ctx_1) = test_st_executor(128);
-
-    let (leader_ot_send_0, follower_ot_recv_0) = ideal_ot();
-    let (follower_ot_send_0, leader_ot_recv_0) = ideal_ot();
-    let (leader_ot_send_1, follower_ot_recv_1) = ideal_ot();
-    let (follower_ot_send_1, leader_ot_recv_1) = ideal_ot();
-
-    let leader_thread_0 = DEAPThread::new(
-        DEAPRole::Leader,
-        [0u8; 32],
-        leader_ctx_0,
-        leader_ot_send_0,
-        leader_ot_recv_0,
-    );
-    let leader_thread_1 = leader_thread_0
-        .new_thread(leader_ctx_1, leader_ot_send_1, leader_ot_recv_1)
-        .unwrap();
-
-    let follower_thread_0 = DEAPThread::new(
-        DEAPRole::Follower,
-        [0u8; 32],
-        follower_ctx_0,
-        follower_ot_send_0,
-        follower_ot_recv_0,
-    );
-    let follower_thread_1 = follower_thread_0
-        .new_thread(follower_ctx_1, follower_ot_send_1, follower_ot_recv_1)
-        .unwrap();
-
-    // Set up public PMS for testing.
-    let leader_pms = leader_thread_0.new_public_input::<[u8; 32]>("pms").unwrap();
-    let follower_pms = follower_thread_0
-        .new_public_input::<[u8; 32]>("pms")
-        .unwrap();
-
-    leader_thread_0.assign(&leader_pms, pms).unwrap();
-
-    let mut leader = MpcPrf::new(
-        PrfConfig::builder().role(Role::Leader).build().unwrap(),
-        leader_thread_0,
-        leader_thread_1,
-    );
-    let mut follower = MpcPrf::new(
-        PrfConfig::builder().role(Role::Follower).build().unwrap(),
-        follower_thread_0,
-        follower_thread_1,
-    );
-
-    futures::join!(
-        async {
-            leader.setup(leader_pms).await.unwrap();
-            leader.set_client_random(Some(client_random)).await.unwrap();
-            leader.preprocess().await.unwrap();
-        },
-        async {
-            follower.setup(follower_pms).await.unwrap();
-            follower.set_client_random(None).await.unwrap();
-            follower.preprocess().await.unwrap();
-        }
-);
 }
 
 async fn run_instance<S: AsyncWrite + AsyncRead + Send + Sync + Unpin + 'static>(
@@ -256,6 +183,12 @@ async fn run_instance<S: AsyncWrite + AsyncRead + Send + Sync + Unpin + 'static>
     prover.prove().await?;
     prover.finalize().await?;
 
+    let heap_max_bytes = if memory_profile {
+        Some(dhat::HeapStats::get().max_bytes)
+    } else {
+        None
+    };
+
     Ok(Metrics {
         name,
         upload,
@@ -268,11 +201,7 @@ async fn run_instance<S: AsyncWrite + AsyncRead + Send + Sync + Unpin + 'static>
         runtime: Instant::now().duration_since(start_time).as_secs(),
         uploaded: uploaded.load(Ordering::SeqCst),
         downloaded: downloaded.load(Ordering::SeqCst),
-        heap_max_bytes: if memory_profile {
-            Some(dhat::HeapStats::get().max_bytes)
-        } else {
-            None
-        },
+        heap_max_bytes
     })
 }
 
--- crates/benches/src/lib.rs
+++ crates/benches/src/lib.rs
@@ -3,6 +3,11 @@ pub mod metrics;
 
 use std::{io, process::Command};
 
+use hmac_sha256::{MpcPrf, PrfConfig, Role, Prf};
+use mpz_common::executor::test_st_executor;
+use mpz_garble::{config::Role as DEAPRole, protocol::deap::DEAPThread, Memory};
+use mpz_ot::ideal::ot::ideal_ot;
+
 pub const PROVER_NAMESPACE: &str = "prover-ns";
 pub const PROVER_INTERFACE: &str = "prover-veth";
 pub const PROVER_SUBNET: &str = "10.10.1.0/24";
@@ -252,4 +257,71 @@ fn assign_ip_to_interface(namespace: &str, interface: &str, ip: &str) -> io::Res
         .status()?;
 
     Ok(())
+}
+
+pub async fn preprocess_prf_circuits() {
+    let pms = [42u8; 32];
+    let client_random = [69u8; 32];
+
+    let (leader_ctx_0, follower_ctx_0) = test_st_executor(128);
+    let (leader_ctx_1, follower_ctx_1) = test_st_executor(128);
+
+    let (leader_ot_send_0, follower_ot_recv_0) = ideal_ot();
+    let (follower_ot_send_0, leader_ot_recv_0) = ideal_ot();
+    let (leader_ot_send_1, follower_ot_recv_1) = ideal_ot();
+    let (follower_ot_send_1, leader_ot_recv_1) = ideal_ot();
+
+    let leader_thread_0 = DEAPThread::new(
+        DEAPRole::Leader,
+        [0u8; 32],
+        leader_ctx_0,
+        leader_ot_send_0,
+        leader_ot_recv_0,
+    );
+    let leader_thread_1 = leader_thread_0
+        .new_thread(leader_ctx_1, leader_ot_send_1, leader_ot_recv_1)
+        .unwrap();
+
+    let follower_thread_0 = DEAPThread::new(
+        DEAPRole::Follower,
+        [0u8; 32],
+        follower_ctx_0,
+        follower_ot_send_0,
+        follower_ot_recv_0,
+    );
+    let follower_thread_1 = follower_thread_0
+        .new_thread(follower_ctx_1, follower_ot_send_1, follower_ot_recv_1)
+        .unwrap();
+
+    // Set up public PMS for testing.
+    let leader_pms = leader_thread_0.new_public_input::<[u8; 32]>("pms").unwrap();
+    let follower_pms = follower_thread_0
+        .new_public_input::<[u8; 32]>("pms")
+        .unwrap();
+
+    leader_thread_0.assign(&leader_pms, pms).unwrap();
+
+    let mut leader = MpcPrf::new(
+        PrfConfig::builder().role(Role::Leader).build().unwrap(),
+        leader_thread_0,
+        leader_thread_1,
+    );
+    let mut follower = MpcPrf::new(
+        PrfConfig::builder().role(Role::Follower).build().unwrap(),
+        follower_thread_0,
+        follower_thread_1,
+    );
+
+    futures::join!(
+        async {
+            leader.setup(leader_pms).await.unwrap();
+            leader.set_client_random(Some(client_random)).await.unwrap();
+            leader.preprocess().await.unwrap();
+        },
+        async {
+            follower.setup(follower_pms).await.unwrap();
+            follower.set_client_random(None).await.unwrap();
+            follower.preprocess().await.unwrap();
+        }
+);
 }
