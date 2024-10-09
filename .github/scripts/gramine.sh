863f183573e1fbd3bcf2b299e4f446206eb4ca96
mac <ryan.macarthur@gmail.com>
2024-10-09 23:00:25 +0200

    Update gramine.sh

--- .github/scripts/gramine.sh
+++ .github/scripts/gramine.sh
@@ -1,4 +1,13 @@
 #/bin/sh
+# this is to be ran in a docker container via an github action that has gramine set-up already e.g.,
+# notaryserverbuilds.azurecr.io/builder/gramine
+# with sgx hardware:
+# ./gramine.sh sgx
+#
+# without:
+# ./gramine.sh 
+##
+
 if [ -z "$1" ]
   then
      run='gramine-direct  notary-server &'
@@ -7,9 +16,12 @@ if [ -z "$1" ]
       run='gramine-sgx  notary-server &'
 fi
 
+
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
 apt install libssl-dev
+
 gramine-sgx-gen-private-key
 SGX=1 make
 gramine-sgx-sign -m notary-server.manifest -o notary-server.sgx
@@ -20,8 +32,13 @@ echo "\`\`\`${mr_enclave}\`\`\`" | tee >> $GITHUB_STEP_SUMMARY
 mkdir config && cp config.yaml config && mkdir /tee && cp -R ../tee/* /tee/ && cd /tee &&
 eval "$run"
 sleep 5
-quote=$(curl 127.0.0.1:7047/info | jq .quote.rawQuote)
-echo $quote
-echo "quote=$quote" >> $GITHUB_OUTPUT
-echo "#### 🔒 signed quote ${quote}" | tee >> $GITHUB_STEP_SUMMARY
-echo "${quote}" | tee >> $GITHUB_STEP_SUMMARY
+
+if [ "$1" ]; then
+    curl 127.0.0.1:7047/info
+else
+    quote=$(curl 127.0.0.1:7047/info | jq .quote.rawQuote)
+    echo $quote
+    echo "quote=$quote" >> $GITHUB_OUTPUT
+    echo "#### 🔒 signed quote ${quote}" | tee >> $GITHUB_STEP_SUMMARY
+    echo "${quote}" | tee >> $GITHUB_STEP_SUMMARY
+fi
