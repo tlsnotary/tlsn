1d4c50f804d44e89a923ef43febd266b456102d1
Kimani <104557098+anthonykimani@users.noreply.github.com>
2024-10-11 06:56:37 +0300

    feat(notary): support reading config values from CLI and env var (#605)
    * feat: supports reading config values from CLI
    
    * chore: adds config lib to cargo.toml, uses server default config values instead, removes validations in settings.rs
    
    * chore: tries to load YAML file
    
    * chore: tries to load YAML file
    
    * fix: loads config.yaml properly, refactors code
    
    * fix: removes .idea folder and moves config lib to notary-server cargo.toml
    
    * feat: uses serde-aux to deserialize env vars port and tls-enabled from string and restores &cli_fields.config_file path and debug log
    
    * fix: parses int and bool using try-parsing instead of serde-aux and removes unnecessary whitespaces
    
    * chore: converts config to snake_case for consistency
    
    * doc: adds configuration documentation, code comments and fixes linting errors
    
    * fix: fixes ci linting formatting
    
    * fix: fixes ci linting formatting
    
    * fix: adjusts formatting for settings.rs and minor adjustments to documentation
    
    * fix: uses cargo nightly to format correctly
    
    ---------
    
    Co-authored-by: yuroitaki <25913766+yuroitaki@users.noreply.github.com>

--- crates/notary/server/src/main.rs
+++ crates/notary/server/src/main.rs
@@ -1,25 +1,23 @@
 use eyre::{eyre, Result};
+use notary_server::{init_tracing, run_server, CliFields, NotaryServerError, Settings};
 use structopt::StructOpt;
 use tracing::debug;
 
-use notary_server::{
-    init_tracing, parse_config_file, run_server, CliFields, NotaryServerError,
-    NotaryServerProperties,
-};
-
 #[tokio::main]
 async fn main() -> Result<(), NotaryServerError> {
-    // Load command line arguments which contains the config file location
+    // Load command line arguments
     let cli_fields: CliFields = CliFields::from_args();
-    let config: NotaryServerProperties = parse_config_file(&cli_fields.config_file)?;
+
+    let settings =
+        Settings::new(&cli_fields).map_err(|err| eyre!("Failed to load settings: {}", err))?;
 
     // Set up tracing for logging
-    init_tracing(&config).map_err(|err| eyre!("Failed to set up tracing: {err}"))?;
+    init_tracing(&settings.config).map_err(|err| eyre!("Failed to set up tracing: {err}"))?;
 
-    debug!(?config, "Server config loaded");
+    debug!(?settings.config, "Server config loaded");
 
     // Run the server
-    run_server(&config).await?;
+    run_server(&settings.config).await?;
 
     Ok(())
 }
