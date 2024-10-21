3a8f89affc66a3a4b2bee2cfd2b889ecbe4bf13c
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-10-21 19:59:32 +0200

    Better error reporting

--- crates/notary/server/build.rs
+++ crates/notary/server/build.rs
@@ -1,30 +1,49 @@
-use git2::{Repository, StatusOptions};
-use std::env;
+use chrono::DateTime;
+use git2::{Commit, Repository, StatusOptions};
+use std::{env, error::Error};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     if env::var("GIT_COMMIT_HASH").is_err() {
-        let repo = Repository::discover(".").expect("Failed to open repository");
-        let commit_hash = get_commit_hash(&repo).expect("Failed to get commit hash");
-        let has_changes = check_local_changes(&repo).expect("Failed to check local changes");
-
-        let change_suffix = if has_changes {
-            " (with local changes)"
-        } else {
-            ""
+        match get_commithash_with_dirty_suffix() {
+            Ok(commit_hash_with_suffix) => {
+                // Pass value as env var to the notary server
+                println!("cargo:rustc-env=GIT_COMMIT_HASH={commit_hash_with_suffix}");
+            }
+            Err(e) => {
+                eprintln!("Failed to get commit hash in notary server build");
+                eprintln!("Fix the error or configure GIT_COMMIT_HASH as environment variable");
+                return Err(e.message().into());
+                // return Err(e.to_string().into());
+            }
         };
+    }
+    Ok(())
+}
 
-        // Pass value as env var to the notary server
-        println!(
-            "cargo:rustc-env=GIT_COMMIT_HASH={}{}",
-            commit_hash, change_suffix
-        );
+fn get_commithash_with_dirty_suffix() -> Result<String, git2::Error> {
+    let repo = Repository::discover(".")?;
+    let commit = get_commit(&repo)?;
+    let commit_hash = commit.id().to_string();
+    let _timestamp = get_commit_timestamp(&commit)?;
+    let has_changes = check_local_changes(&repo)?;
+
+    if has_changes {
+        Ok(format!("{commit_hash} (with local changes)"))
+    } else {
+        Ok(commit_hash)
     }
 }
 
-fn get_commit_hash(repo: &Repository) -> Result<String, git2::Error> {
+fn get_commit(repo: &Repository) -> Result<Commit, git2::Error> {
     let head = repo.head()?;
-    let commit = head.peel_to_commit()?;
-    Ok(commit.id().to_string())
+    head.peel_to_commit()
+}
+
+fn get_commit_timestamp(commit: &Commit) -> Result<String, git2::Error> {
+    let timestamp = commit.time().seconds();
+    let date_time = DateTime::from_timestamp(timestamp, 0)
+        .ok_or_else(|| git2::Error::from_str("Invalid timestamp"))?;
+    Ok(date_time.to_rfc2822())
 }
 
 fn check_local_changes(repo: &Repository) -> Result<bool, git2::Error> {
