4445521240a25f57fee5714fa84930d34f7757a3
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-10-21 19:58:59 +0200

    fix: Server fixture now returns valid json
    * return json instead of string
    * removed trailing whitespace
    * use a constant for the default port
    * give binary a better name

--- crates/server-fixture/server/src/lib.rs
+++ crates/server-fixture/server/src/lib.rs
@@ -5,9 +5,9 @@ use std::{
 
 use axum::{
     extract::{Query, State},
-    response::{Html, Json},
+    response::Html,
     routing::get,
-    Router,
+    Json, Router,
 };
 use futures::{channel::oneshot, AsyncRead, AsyncWrite};
 use futures_rustls::{
@@ -22,10 +22,13 @@ use hyper::{
 };
 use hyper_util::rt::TokioIo;
 
+use serde_json::Value;
 use tokio_util::compat::FuturesAsyncReadCompatExt;
 use tower_service::Service;
 
 use tlsn_server_fixture_certs::*;
+
+pub const DEFAULT_FIXTURE_PORT: u16 = 3000;
 
 struct AppState {
     shutdown: Option<oneshot::Sender<()>>,
@@ -94,10 +97,18 @@ async fn bytes(
     Ok(Bytes::from(vec![0x42u8; size]))
 }
 
+/// parse the JSON data from the file content
+fn get_json_value(filecontent: &str) -> Result<Json<Value>, StatusCode> {
+    Ok(Json(serde_json::from_str(filecontent).map_err(|e| {
+        eprintln!("Failed to parse JSON data: {}", e);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?))
+}
+
 async fn json(
     State(state): State<Arc<Mutex<AppState>>>,
     Query(params): Query<HashMap<String, String>>,
-) -> Result<Json<&'static str>, StatusCode> {
+) -> Result<Json<Value>, StatusCode> {
     let size = params
         .get("size")
         .and_then(|size| size.parse::<usize>().ok())
@@ -108,9 +119,9 @@ async fn json(
     }
 
     match size {
-        1 => Ok(Json(include_str!("data/1kb.json"))),
-        4 => Ok(Json(include_str!("data/4kb.json"))),
-        8 => Ok(Json(include_str!("data/8kb.json"))),
+        1 => get_json_value(include_str!("data/1kb.json")),
+        4 => get_json_value(include_str!("data/4kb.json")),
+        8 => get_json_value(include_str!("data/8kb.json")),
         _ => Err(StatusCode::NOT_FOUND),
     }
 }
