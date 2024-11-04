bf539488a3626381933deadaca18ca5894b8a73b
yuroitaki <yuroitaki@users.noreply.github.com>
2024-11-04 06:00:43 +0000

    deploy: c10c9155a76dd1ddc7546dfddd368f454aedbf4d

--- tlsn_core/fixtures/sidebar-items.js
+++ tlsn_core/fixtures/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["encoder_seed","encoding_provider","notary_signing_key"],"struct":["ChaChaProvider","ConnectionFixture"]};
+window.SIDEBAR_ITEMS = {"fn":["attestation_fixture","encoder_seed","encoding_provider","notary_signing_key","request_fixture"],"struct":["ChaChaProvider","ConnectionFixture","RequestFixture"]};
