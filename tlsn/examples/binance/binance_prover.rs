892d377c86a1c58df568289a5b3991afd47cb32f
mhchia <kevin.mh.chia@gmail.com>
2024-11-08 16:55:25 +0700

    add todo

--- tlsn/examples/binance/binance_prover.rs
+++ tlsn/examples/binance/binance_prover.rs
@@ -33,6 +33,8 @@ const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KH
 // Maximum number of bytes that can be sent from prover to server
 const MAX_SENT_DATA: usize = 1 << 12;
 // Maximum number of bytes that can be received by prover from server
+// TODO: determine the actual size of the response by querying the server with the same request first.
+// Right now it's fixed.
 const MAX_RECV_DATA: usize = 1 << 14;
 
 use std::{env, str};
