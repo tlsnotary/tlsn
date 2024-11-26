a4106ac729817c8252d68ccdf67e73522e826d61
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-11-26 10:48:32 +0100

    Improved package: add description, repo information etc
    + improved README for npmjs.com

--- crates/wasm/build.sh
+++ crates/wasm/build.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
+
+# This crate must be built using the nightly Rust compiler with specific flags.
+# This script automates the build process.
+
 set -e
 
 # Clean up older builds
 rm -rf pkg
 
 # Build tlsn_wasm package
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
-    rustup run nightly \
-    wasm-pack build --target web . -- -Zbuild-std=panic_abort,std
+wasm-pack build --target web .
 
 # Patch tlsn_wasm.js import in workerHelpers.worker.js
-file=$(ls ./pkg/snippets/wasm-bindgen-rayon-*/src/workerHelpers.worker.js)
+file=$(find ./pkg/snippets -name "workerHelpers.worker.js" -print -quit)
+if [ -z "$file" ]; then
+    echo "Error: workerHelpers.worker.js not found"
+    find pkg
+    exit 1
+fi
 temp=$(mktemp)
 sed 's|../../../|../../../tlsn_wasm.js|' "$file" >"$temp" && mv "$temp" "$file"
 
