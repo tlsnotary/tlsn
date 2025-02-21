9bd3f52d5eb1a115882960f6f6df8298ae68c4b2
heeckhau <heeckhau@users.noreply.github.com>
2025-02-21 08:05:28 +0000

    deploy: 119ae4b2a875e91da089d77006b09766d5ecf861

--- trait.impl/core/future/future/trait.Future.js
+++ trait.impl/core/future/future/trait.Future.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["tlsn_prover",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"tlsn_prover/struct.ProverFuture.html\" title=\"struct tlsn_prover::ProverFuture\">ProverFuture</a>"]]]]);
+    var implementors = Object.fromEntries([["tlsn_prover",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"tlsn_prover/struct.ProverFuture.html\" title=\"struct tlsn_prover::ProverFuture\">ProverFuture</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
