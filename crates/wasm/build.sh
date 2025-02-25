76f986d901451af6d802116b78ecc23f5f5f7106
sinu <65924192+sinui0@users.noreply.github.com>
2025-02-25 11:26:34 -0800

    fix: remove wasm bundling hack

--- crates/wasm/build.sh
+++ crates/wasm/build.sh
@@ -11,17 +11,3 @@ rm -rf pkg
 # Build tlsn_wasm package
 wasm-pack build --target web .
 
-# Patch tlsn_wasm.js import in workerHelpers.worker.js
-file=$(find ./pkg/snippets -name "workerHelpers.worker.js" -print -quit)
-if [ -z "$file" ]; then
-    echo "Error: workerHelpers.worker.js not found"
-    find pkg
-    exit 1
-fi
-temp=$(mktemp)
-sed 's|../../../|../../../tlsn_wasm.js|' "$file" >"$temp" && mv "$temp" "$file"
-
-# Add snippets directory to package.json
-file="pkg/package.json"
-temp=$(mktemp)
-jq '.files += ["snippets/"]' "$file" >"$temp" && mv "$temp" "$file"
