5246beabf5ab953c17f1c4c8e13fbdabeda0a37f
sinu.eth <65924192+sinui0@users.noreply.github.com>
2025-03-07 10:57:25 -0800

    chore(wasm): bump web spawn to 0.2 (#714)

--- crates/wasm-test-runner/run.sh
+++ crates/wasm-test-runner/run.sh
@@ -5,5 +5,5 @@ cd "$(dirname "$0")"
 
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--max-memory=4294967296' \
     rustup run nightly \
-    wasm-pack build ../wasm --target web --no-pack --out-dir=../wasm-test-runner/static/generated -- -Zbuild-std=panic_abort,std --features test &&
-    RUST_LOG=info cargo run --release
+    wasm-pack build ../wasm --target web --no-pack --out-dir=../wasm-test-runner/static/generated -- -Zbuild-std=panic_abort,std --features test,no-bundler &&
+    RUST_LOG=debug cargo run --release
