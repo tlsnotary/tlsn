67c515a6bf6bb08a3fb82ce158dc23250db23800
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2025-03-11 11:47:30 +0100

    ci: script to patch imports in the tlsn-wasm build result

--- crates/wasm/build.sh
+++ crates/wasm/build.sh
@@ -11,3 +11,20 @@ rm -rf pkg
 # Build tlsn_wasm package
 wasm-pack build --target web .
 
+# Patch tlsn_wasm.js import in spawn.js snippet and copy it to the main folder
+file=$(find ./pkg/snippets -name "spawn.js" -print -quit)
+if [ -z "$file" ]; then
+    echo "Error: spawn.js snippet not found"
+    find pkg
+    exit 1
+fi
+temp=$(mktemp)
+sed 's|../../..|../../../tlsn_wasm.js|' "$file" >"$temp" && mv "$temp" "$file"
+cp ${file} ./pkg
+
+# Add all files and snippets directory to package.json
+file="pkg/package.json"
+temp=$(mktemp)
+jq '.files += ["tlsn_wasm_bg.wasm.d.ts"]' "$file" >"$temp" && mv "$temp" "$file"
+jq '.files += ["spawn.js"]' "$file" >"$temp" && mv "$temp" "$file"
+jq '.files += ["snippets/"]' "$file" >"$temp" && mv "$temp" "$file"
