cda1736f2d847dee08aaf50cbe8c08fe3ce44ba4
sinui0 <sinui0@users.noreply.github.com>
2025-03-19 17:18:52 +0000

    deploy: efca281222113affcff7a391ed8585f138cdf129

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["tlsn_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"tlsn_core/connection/struct.ServerName.html\" title=\"struct tlsn_core::connection::ServerName\">ServerName</a>"]]]]);
+    var implementors = Object.fromEntries([["tlsn_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.1/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"tlsn_core/connection/struct.ServerName.html\" title=\"struct tlsn_core::connection::ServerName\">ServerName</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
