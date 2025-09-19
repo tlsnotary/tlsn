9697749038a3355813b3e011dcf42582e9b77568
heeckhau <heeckhau@users.noreply.github.com>
2025-09-19 14:56:59 +0000

    deploy: 0539268da7a86e132beffea6c1585402a9a94a8b

--- trait.impl/wasm_bindgen/__rt/trait.VectorIntoJsValue.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["tlsn_wasm",[["impl VectorIntoJsValue for <a class=\"struct\" href=\"tlsn_wasm/prover/struct.JsProver.html\" title=\"struct tlsn_wasm::prover::JsProver\">JsProver</a>"],["impl VectorIntoJsValue for <a class=\"struct\" href=\"tlsn_wasm/verifier/struct.JsVerifier.html\" title=\"struct tlsn_wasm::verifier::JsVerifier\">JsVerifier</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[337]}
